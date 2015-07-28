824df10f083cfc6bc508b80cd7b8c30d7ad54836
Flavio W. Brasil <fwbrasil@gmail.com>
2015-07-29 00:24:22 +0200

    enable the tests that don't use partials

--- quill-jdbc/src/test/scala/io/getquill/Test.scala
+++ quill-jdbc/src/test/scala/io/getquill/Test.scala
@@ -1,131 +1,131 @@
-//package io.getquill
-//
-//import language.experimental.macros
-//import io.getquill.jdbc.JdbcSource
-//import java.sql.ResultSet
-//
-//case class Person(id: Long, name: String, surname: String, age: Int)
-//case class Address(id: Long, personId: Long, streetId: Long, number: Int)
-//case class Street(id: Long, name: String, city: String)
-//
-//object Test extends App {
-//
-//  object db extends JdbcSource
-//
-//  def q1 =
-//    query {
-//      from[Person].filter(p => p.name == p.surname).map(p => (p.name, p.age))
-//    }
-//  db.run(q1)
-//
-//  def q2 =
-//    query {
-//      for {
-//        p <- from[Person] if (p.name == null)
-//        a <- from[Address] if (a.personId == p.id)
-//      } yield {
-//        a
-//      }
-//    }
-//
-//  db.run(q2)
-//
-//  def q3 =
-//    query {
-//      for {
-//        a <- q2
-//        s <- from[Street] if (a.streetId == s.id)
-//      } yield {
-//        a
-//      }
-//    }
-//  db.run(q3)
-//
-//  val byName = partial {
+package io.getquill
+
+import language.experimental.macros
+import io.getquill.jdbc.JdbcSource
+import java.sql.ResultSet
+
+case class Person(id: Long, name: String, surname: String, age: Int)
+case class Address(id: Long, personId: Long, streetId: Long, number: Int)
+case class Street(id: Long, name: String, city: String)
+
+object Test extends App {
+
+  object db extends JdbcSource
+
+  def q1 =
+    quote {
+      from[Person].filter(p => p.name == p.surname).map(p => (p.name, p.age))
+    }
+  db.run(q1)
+
+  def q2 =
+    quote {
+      for {
+        p <- from[Person] if (p.name == null)
+        a <- from[Address] if (a.personId == p.id)
+      } yield {
+        a
+      }
+    }
+
+  db.run(q2)
+
+  def q3 =
+    quote {
+      for {
+        a <- q2
+        s <- from[Street] if (a.streetId == s.id)
+      } yield {
+        a
+      }
+    }
+  db.run(q3)
+
+//  val byName = quote {
 //    (name: String) => from[Person].filter(_.name == name)
 //  }
 //
 //  val q4 =
-//    query {
+//    quote {
 //      byName("jesus")
 //    }
 //  db.run(q4)
-//
-//  val byFullName = partial {
+
+//  val byFullName = quote {
 //    (name: String, surname: String) => byName(name).filter(_.surname == surname)
 //  }
 //
-//  val q5 = query {
+//  val q5 = quote {
 //    byFullName("flavio", "brasil")
 //  }
 //  db.run(q5)
-//
-//  val nameEqualsSurname = partial {
+
+//  val nameEqualsSurname = quote {
 //    (p: Person) => p.name == p.surname
 //  }
 //
-//  val q6 = query {
+//  val q6 = quote {
 //    from[Person].filter(nameEqualsSurname(_))
 //  }
 //  db.run(q6)
-//
-//  val nameIs = partial {
+
+//  val nameIs = quote {
 //    (p: Person, name: String) => p.name == name
 //  }
 //
-//  val q7 = query {
+//  val q7 = quote {
 //    from[Person].filter(nameIs(_, "flavio"))
 //  }
 //  db.run(q7)
-//
-//  val names = query {
-//    from[Person].map(_.name)
-//  }
-//
-//  val q8 = query {
-//    for {
-//      name <- names
-//      p <- from[Person] if (p.name == name)
-//    } yield {
-//      (p.name, p.age)
-//    }
-//  }
-//  db.run(q8)
-//
-//  val q9 = query {
-//    from[Address].map(_.personId)
-//  }
-//  db.run(q9)
-//
-//  val q10 =
-//    query {
+
+  val names = quote {
+    from[Person].map(_.name)
+  }
+
+  val q8 = quote {
+    for {
+      name <- names
+      p <- from[Person] if (p.name == name)
+    } yield {
+      (p.name, p.age)
+    }
+  }
+  db.run(q8)
+
+  val q9 = quote {
+    from[Address].map(_.personId)
+  }
+  db.run(q9)
+
+  val q10 =
+    quote {
+      for {
+        p1 <- from[Person]
+        p2 <- from[Person] if (p1.name == p2.name)
+      } yield {
+        p2
+      }
+    }
+  db.run(q10)
+
+  val personAndAddress =
+    quote {
+      for {
+        p <- from[Person]
+        a <- from[Address] if (a.personId == p.id)
+      } yield {
+        (p, a)
+      }
+    }
+
+//  val q11 =
+//    quote {
 //      for {
-//        p1 <- from[Person]
-//        p2 <- from[Person] if (p1.name == p2.name)
+//        (pp, aa) <- personAndAddress
+//        s <- from[Street] if (aa.streetId == s.id)
 //      } yield {
-//        p2
+//        (pp, aa, s.city)
 //      }
 //    }
-//  db.run(q10)
-//
-//  val personAndAddress =
-//    query {
-//      for {
-//        p <- from[Person]
-//        a <- from[Address] if (a.personId == p.id)
-//      } yield {
-//        (p, a)
-//      }
-//    }
-//
-////  val q11 =
-////    query {
-////      for {
-////        (pp, aa) <- personAndAddress
-////        s <- from[Street] if (aa.streetId == s.id)
-////      } yield {
-////        (pp, aa, s.city)
-////      }
-////    }
-////  db.run(q11)
-//}
+//  db.run(q11)
+}
