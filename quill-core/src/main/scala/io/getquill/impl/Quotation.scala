b69a71fb592c577be31cb6c3ff70d36e3492c81a
Flavio W. Brasil <fwbrasil@gmail.com>
2015-08-16 10:23:42 +0200

    fail if a quotation has free variables

--- quill-core/src/main/scala/io/getquill/impl/Quotation.scala
+++ quill-core/src/main/scala/io/getquill/impl/Quotation.scala
@@ -3,7 +3,7 @@ package io.getquill.impl
 import scala.annotation.StaticAnnotation
 import scala.reflect.macros.whitebox.Context
 
-import io.getquill.util.Messages.RichContext
+import io.getquill.util.Messages._
 
 trait Quoted[+T]
 
@@ -14,7 +14,8 @@ trait Quotation {
 
   case class QuotedTree(tree: Any) extends StaticAnnotation
 
-  def quote[T: WeakTypeTag](body: c.Expr[T]) =
+  def quote[T: WeakTypeTag](body: c.Expr[T]) = {
+    verifyFreeVariables(body.tree)
     q"""
       new ${c.weakTypeOf[Quoted[T]]} {
         @${c.weakTypeOf[QuotedTree]}(${body.tree})
@@ -22,6 +23,7 @@ trait Quotation {
         override def toString = ${body.tree.toString}
       }
     """
+  }
 
   def unquote[T](quoted: c.Expr[Quoted[T]]) =
     unquoteTree(quoted.tree)
@@ -36,4 +38,31 @@ trait Quotation {
     annotation.tree.children.lastOption
       .getOrElse(c.fail(s"Can't find the QuotedTree body at $annotation"))
   }
+
+  private def verifyFreeVariables(tree: Tree) =
+    freeVariables(tree) match {
+      case Nil  =>
+      case vars => c.fail(s"A quotation must not have references to free variables. Found: ${vars.mkString(", ")}")
+    }
+
+  private def freeVariables(tree: Tree, known: List[Symbol] = List()): List[String] =
+    tree match {
+      case Select(This(TypeName(tpe)), TermName(name)) =>
+        List(name)
+      case i: Ident if (isVariable(i.symbol) && i.toString != "_" && !known.contains(i.symbol)) =>
+        List(i.toString)
+      case q"(..$params) => $body" =>
+        freeVariables(body, known ++ params.map(_.symbol))
+      case q"new { def apply[..$t1](...$params) = $body }" =>
+        freeVariables(body, known ++ params.flatten.map(_.symbol))
+      case q"$tuple match { case (..$params) => $body }" =>
+        freeVariables(body, known ++ params.map(_.symbol))
+      case tree if (tree.children.nonEmpty) =>
+        tree.children.map(freeVariables(_, known)).flatten
+      case other =>
+        List()
+    }
+
+  private def isVariable(s: Symbol) =
+    !s.isPackage && !s.isMethod && !s.isModule && !s.isClass && !s.isType
 }
