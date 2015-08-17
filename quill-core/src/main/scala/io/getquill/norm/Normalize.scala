f23f109550c2c0e254b1b0be5e38da7143336ed3
Flavio W. Brasil <fwbrasil@gmail.com>
2015-08-17 14:38:34 +0200

    refactor normalization

--- quill-core/src/main/scala/io/getquill/norm/Normalize.scala
+++ quill-core/src/main/scala/io/getquill/norm/Normalize.scala
@@ -13,8 +13,9 @@ import io.getquill.ast.Query
 import io.getquill.ast.Table
 import io.getquill.ast.Update
 import io.getquill.norm.capture.AvoidCapture
+import io.getquill.ast.SimpleTransformer
 
-object Normalize {
+object Normalize extends SimpleTransformer {
 
   def apply(a: Action): Action =
     a match {
@@ -26,93 +27,83 @@ object Normalize {
         Delete(apply(query))
     }
 
-  def apply(q: Query) =
-    BetaReduction(norm(AvoidCapture(q)))
+  override def apply(q: Query): Query =
+    AvoidCapture(q) match {
 
-  private def norm(q: Query): Query = {
-    import io.getquill.util.Show._
-    import io.getquill.ast.ExprShow._
-    println(q.show)
-    val r =
-      q match {
+      // ************Symbolic***************
 
-        // ************Symbolic***************
+      // a.flatMap(b => c.map(d => e)).flatMap(f => g)
+      //    a.flatMap(b => c).flatMap(d => g[f := e])
+      case FlatMap(FlatMap(a, b, Map(c, d, e)), f, g) =>
+        val gr = BetaReduction(g, f -> e)
+        apply(FlatMap(FlatMap(a, b, c), d, gr))
 
-        // a.flatMap(b => c.map(d => e)).flatMap(f => g)
-        //    a.flatMap(b => c).flatMap(d => g[f := e])
-        case FlatMap(FlatMap(a, b, Map(c, d, e)), f, g) =>
-          val gr = BetaReduction(g, f -> e)
-          norm(FlatMap(FlatMap(a, b, c), d, gr))
+      // a.map(b => c).flatMap(d => e) =>
+      //    a.flatMap(b => e[d := c])
+      case FlatMap(Map(a, b, c), d, e) =>
+        val er = BetaReduction(e, d -> c)
+        apply(FlatMap(a, b, er))
 
-        // a.map(b => c).flatMap(d => e) =>
-        //    a.flatMap(b => e[d := c])
-        case FlatMap(Map(a, b, c), d, e) =>
-          val er = BetaReduction(e, d -> c)
-          norm(FlatMap(a, b, er))
+      // a.map(b => c).filter(d => e) =>
+      //    a.filter(b => e[d := c]).map(b => c)
+      case Filter(Map(a, b, c), d, e) =>
+        val er = BetaReduction(e, d -> c)
+        apply(Map(Filter(a, b, er), b, c))
 
-        // a.map(b => c).filter(d => e) =>
-        //    a.filter(b => e[d := c]).map(b => c)
-        case Filter(Map(a, b, c), d, e) =>
-          val er = BetaReduction(e, d -> c)
-          norm(Map(Filter(a, b, er), b, c))
+      // a.map(b => c).map(d => e) =>
+      //    a.map(b => e[d := c])
+      case Map(Map(a, b, c), d, e) =>
+        val er = BetaReduction(e, d -> c)
+        apply(Map(a, b, er))
 
-        // a.map(b => c).map(d => e) =>
-        //    a.map(b => e[d := c])
-        case Map(Map(a, b, c), d, e) =>
-          val er = BetaReduction(e, d -> c)
-          norm(Map(a, b, er))
+      // a.flatMap(b => c).flatMap(d => e) =>
+      //     a.flatMap(b => c.flatMap(d => e))
+      case FlatMap(FlatMap(a, b, c), d, e) =>
+        apply(FlatMap(a, b, FlatMap(c, d, e)))
 
-        // a.flatMap(b => c).flatMap(d => e) =>
-        //     a.flatMap(b => c.flatMap(d => e))
-        case FlatMap(FlatMap(a, b, c), d, e) =>
-          norm(FlatMap(a, b, FlatMap(c, d, e)))
+      // a.filter(b => c).flatMap(d => e.map(f => g)) =>
+      //     a.flatMap(d => e.filter(temp => c[b := d]).map(f => g))
+      case FlatMap(Filter(a, b, c), d, Map(e, f, g)) =>
+        val cr = BetaReduction(c, b -> d)
+        apply(FlatMap(a, d, Map(Filter(e, Ident("temp"), cr), f, g)))
 
-        // a.filter(b => c).flatMap(d => e.map(f => g)) =>
-        //     a.flatMap(d => e.filter(temp => c[b := d]).map(f => g))
-        case FlatMap(Filter(a, b, c), d, Map(e, f, g)) =>
-          val cr = BetaReduction(c, b -> d)
-          norm(FlatMap(a, d, Map(Filter(e, Ident("temp"), cr), f, g)))
+      // a.filter(b => c).flatMap(d => e) =>
+      //     a.flatMap(d => e.filter(temp => c[b := d]))
+      case FlatMap(Filter(a, b, c), d, e) =>
+        val cr = BetaReduction(c, b -> d)
+        apply(FlatMap(a, d, Filter(e, Ident("temp"), cr)))
 
-        // a.filter(b => c).flatMap(d => e) =>
-        //     a.flatMap(d => e.filter(temp => c[b := d]))
-        case FlatMap(Filter(a, b, c), d, e) =>
-          val cr = BetaReduction(c, b -> d)
-          norm(FlatMap(a, d, Filter(e, Ident("temp"), cr)))
+      // ************AdHoc***************
 
-        // ************AdHoc***************
+      // a.filter(b => c).filter(d => e) =>
+      //    a.filter(b => c && e[d := b])
+      case Filter(Filter(a, b, c), d, e) =>
+        val er = BetaReduction(e, d -> b)
+        apply(Filter(a, b, BinaryOperation(c, `&&`, er)))
 
-        // a.filter(b => c).filter(d => e) =>
-        //    a.filter(b => c && e[d := b])
-        case Filter(Filter(a, b, c), d, e) =>
-          val er = BetaReduction(e, d -> b)
-          norm(Filter(a, b, BinaryOperation(c, `&&`, er)))
+      // a.flatMap(b => c).filter(d => e) =>
+      //    a.flatMap(b => c.filter(temp => e[d := b]))
+      case Filter(FlatMap(a, b, c), d, e) =>
+        val er = BetaReduction(e, d -> b)
+        apply(FlatMap(a, b, Filter(c, Ident("temp"), er)))
 
-        // a.flatMap(b => c).filter(d => e) =>
-        //    a.flatMap(b => c.filter(temp => e[d := b]))
-        case Filter(FlatMap(a, b, c), d, e) =>
-          val er = BetaReduction(e, d -> b)
-          norm(FlatMap(a, b, Filter(c, Ident("temp"), er)))
+      // ************Recursion***************
 
-        // ************Recursion***************
-
-        case FlatMap(a, b, c) =>
-          (norm(a), norm(c)) match {
-            case (`a`, `c`) => FlatMap(a, b, c)
-            case (a, c)     => norm(FlatMap(a, b, c))
-          }
-        case Filter(a, b, c) =>
-          norm(a) match {
-            case `a` => Filter(a, b, c)
-            case a   => norm(Filter(a, b, c))
-          }
-        case Map(a, b, c) =>
-          norm(a) match {
-            case `a` => Map(a, b, c)
-            case a   => norm(Map(a, b, c))
-          }
-        case t: Table => t
-      }
-    //    println(s"${q.show} => \n\t\t ${r.show}")
-    r
-  }
+      case FlatMap(a, b, c) =>
+        (apply(a), apply(c)) match {
+          case (`a`, `c`) => FlatMap(a, b, c)
+          case (a, c)     => apply(FlatMap(a, b, c))
+        }
+      case Filter(a, b, c) =>
+        apply(a) match {
+          case `a` => Filter(a, b, c)
+          case a   => apply(Filter(a, b, c))
+        }
+      case Map(a, b, c) =>
+        apply(a) match {
+          case `a` => Map(a, b, c)
+          case a   => apply(Map(a, b, c))
+        }
+      case t: Table => t
+    }
 }
