ee4e49ca763323f3454ff7a914dbb274477162bd
fwbrasil <fwbrasil@gmail.com>
2015-08-25 13:38:54 +0200

    BetaReductionSpec

--- quill-core/src/test/scala/io/getquill/ast/StatelessTransformerSpec.scala
+++ quill-core/src/test/scala/io/getquill/ast/StatelessTransformerSpec.scala
@@ -13,96 +13,98 @@ class StatelessTransformerSpec extends Spec {
       replace.toMap.getOrElse(e, super.apply(e))
   }
 
-  "query" - {
-    "entity" in {
-      val ast: Ast = Entity("a")
-      Subject()(ast) mustEqual ast
+  "transforms asts" - {
+    "query" - {
+      "entity" in {
+        val ast: Ast = Entity("a")
+        Subject()(ast) mustEqual ast
+      }
+      "filter" in {
+        val ast: Ast = Filter(Ident("a"), Ident("b"), Ident("c"))
+        Subject(Ident("a") -> Ident("a'"), Ident("b") -> Ident("b'"), Ident("c") -> Ident("c'"))(ast) mustEqual
+          Filter(Ident("a'"), Ident("b"), Ident("c'"))
+      }
+      "map" in {
+        val ast: Ast = Map(Ident("a"), Ident("b"), Ident("c"))
+        Subject(Ident("a") -> Ident("a'"), Ident("b") -> Ident("b'"), Ident("c") -> Ident("c'"))(ast) mustEqual
+          Map(Ident("a'"), Ident("b"), Ident("c'"))
+      }
+      "flatMap" in {
+        val ast: Ast = FlatMap(Ident("a"), Ident("b"), Ident("c"))
+        Subject(Ident("a") -> Ident("a'"), Ident("b") -> Ident("b'"), Ident("c") -> Ident("c'"))(ast) mustEqual
+          FlatMap(Ident("a'"), Ident("b"), Ident("c'"))
+      }
     }
-    "filter" in {
-      val ast: Ast = Filter(Ident("a"), Ident("b"), Ident("c"))
-      Subject(Ident("a") -> Ident("a'"), Ident("b") -> Ident("b'"), Ident("c") -> Ident("c'"))(ast) mustEqual
-        Filter(Ident("a'"), Ident("b"), Ident("c'"))
-    }
-    "map" in {
-      val ast: Ast = Map(Ident("a"), Ident("b"), Ident("c"))
-      Subject(Ident("a") -> Ident("a'"), Ident("b") -> Ident("b'"), Ident("c") -> Ident("c'"))(ast) mustEqual
-        Map(Ident("a'"), Ident("b"), Ident("c'"))
-    }
-    "flatMap" in {
-      val ast: Ast = FlatMap(Ident("a"), Ident("b"), Ident("c"))
-      Subject(Ident("a") -> Ident("a'"), Ident("b") -> Ident("b'"), Ident("c") -> Ident("c'"))(ast) mustEqual
-        FlatMap(Ident("a'"), Ident("b"), Ident("c'"))
-    }
-  }
 
-  "operation" - {
-    "unary" in {
-      val ast: Ast = UnaryOperation(io.getquill.ast.`!`, Ident("a"))
+    "operation" - {
+      "unary" in {
+        val ast: Ast = UnaryOperation(io.getquill.ast.`!`, Ident("a"))
+        Subject(Ident("a") -> Ident("a'"))(ast) mustEqual
+          UnaryOperation(io.getquill.ast.`!`, Ident("a'"))
+      }
+      "binary" in {
+        val ast: Ast = BinaryOperation(Ident("a"), io.getquill.ast.`&&`, Ident("b"))
+        Subject(Ident("a") -> Ident("a'"), Ident("b") -> Ident("b'"))(ast) mustEqual
+          BinaryOperation(Ident("a'"), io.getquill.ast.`&&`, Ident("b'"))
+      }
+    }
+
+    "value" - {
+      "constant" in {
+        val ast: Ast = Constant("a")
+        Subject()(ast) mustEqual ast
+      }
+      "null" in {
+        val ast: Ast = NullValue
+        Subject()(ast) mustEqual ast
+      }
+      "tuple" in {
+        val ast: Ast = Tuple(List(Ident("a"), Ident("b"), Ident("c")))
+        Subject(Ident("a") -> Ident("a'"), Ident("b") -> Ident("b'"), Ident("c") -> Ident("c'"))(ast) mustEqual
+          Tuple(List(Ident("a'"), Ident("b'"), Ident("c'")))
+      }
+    }
+
+    "action" - {
+      "update" in {
+        val ast: Ast = Update(Ident("a"), List(Assignment("b", Ident("c"))))
+        Subject(Ident("a") -> Ident("a'"), Ident("b") -> Ident("b'"), Ident("c") -> Ident("c'"))(ast) mustEqual
+          Update(Ident("a'"), List(Assignment("b", Ident("c'"))))
+      }
+      "insert" in {
+        val ast: Ast = Insert(Ident("a"), List(Assignment("b", Ident("c"))))
+        Subject(Ident("a") -> Ident("a'"), Ident("b") -> Ident("b'"), Ident("c") -> Ident("c'"))(ast) mustEqual
+          Insert(Ident("a'"), List(Assignment("b", Ident("c'"))))
+      }
+      "delete" in {
+        val ast: Ast = Delete(Ident("a"))
+        Subject(Ident("a") -> Ident("a'"))(ast) mustEqual
+          Delete(Ident("a'"))
+      }
+    }
+
+    "function" in {
+      val ast: Ast = Function(List(Ident("a")), Ident("a"))
+      Subject(Ident("a") -> Ident("b"))(ast) mustEqual
+        Function(List(Ident("a")), Ident("b"))
+    }
+
+    "function apply" in {
+      val ast: Ast = FunctionApply(Ident("a"), List(Ident("b"), Ident("c")))
+      Subject(Ident("a") -> Ident("a'"), Ident("b") -> Ident("b'"), Ident("c") -> Ident("c'"))(ast) mustEqual
+        FunctionApply(Ident("a'"), List(Ident("b'"), Ident("c'")))
+    }
+
+    "ident" in {
+      val ast: Ast = Ident("a")
       Subject(Ident("a") -> Ident("a'"))(ast) mustEqual
-        UnaryOperation(io.getquill.ast.`!`, Ident("a'"))
+        Ident("a'")
     }
-    "binary" in {
-      val ast: Ast = BinaryOperation(Ident("a"), io.getquill.ast.`&&`, Ident("b"))
-      Subject(Ident("a") -> Ident("a'"), Ident("b") -> Ident("b'"))(ast) mustEqual
-        BinaryOperation(Ident("a'"), io.getquill.ast.`&&`, Ident("b'"))
-    }
-  }
 
-  "value" - {
-    "constant" in {
-      val ast: Ast = Constant("a")
-      Subject()(ast) mustEqual ast
-    }
-    "null" in {
-      val ast: Ast = NullValue
-      Subject()(ast) mustEqual ast
-    }
-    "tuple" in {
-      val ast: Ast = Tuple(List(Ident("a"), Ident("b"), Ident("c")))
-      Subject(Ident("a") -> Ident("a'"), Ident("b") -> Ident("b'"), Ident("c") -> Ident("c'"))(ast) mustEqual
-        Tuple(List(Ident("a'"), Ident("b'"), Ident("c'")))
-    }
-  }
-
-  "action" - {
-    "update" in {
-      val ast: Ast = Update(Ident("a"), List(Assignment("b", Ident("c"))))
-      Subject(Ident("a") -> Ident("a'"), Ident("b") -> Ident("b'"), Ident("c") -> Ident("c'"))(ast) mustEqual
-        Update(Ident("a'"), List(Assignment("b", Ident("c'"))))
-    }
-    "insert" in {
-      val ast: Ast = Insert(Ident("a"), List(Assignment("b", Ident("c"))))
-      Subject(Ident("a") -> Ident("a'"), Ident("b") -> Ident("b'"), Ident("c") -> Ident("c'"))(ast) mustEqual
-        Insert(Ident("a'"), List(Assignment("b", Ident("c'"))))
-    }
-    "delete" in {
-      val ast: Ast = Delete(Ident("a"))
+    "property" in {
+      val ast: Ast = Property(Ident("a"), "b")
       Subject(Ident("a") -> Ident("a'"))(ast) mustEqual
-        Delete(Ident("a'"))
+        Property(Ident("a'"), "b")
     }
-  }
-
-  "function" in {
-    val ast: Ast = Function(List(Ident("a")), Ident("a"))
-    Subject(Ident("a") -> Ident("b"))(ast) mustEqual
-      Function(List(Ident("a")), Ident("b"))
-  }
-
-  "function apply" in {
-    val ast: Ast = FunctionApply(Ident("a"), List(Ident("b"), Ident("c")))
-    Subject(Ident("a") -> Ident("a'"), Ident("b") -> Ident("b'"), Ident("c") -> Ident("c'"))(ast) mustEqual
-      FunctionApply(Ident("a'"), List(Ident("b'"), Ident("c'")))
-  }
-
-  "ident" in {
-    val ast: Ast = Ident("a")
-    Subject(Ident("a") -> Ident("a'"))(ast) mustEqual
-      Ident("a'")
-  }
-
-  "property" in {
-    val ast: Ast = Property(Ident("a"), "b")
-    Subject(Ident("a") -> Ident("a'"))(ast) mustEqual
-      Property(Ident("a'"), "b")
   }
 }
---
+++ quill-core/src/test/scala/io/getquill/norm/BetaReductionSpec.scala
@@ -0,0 +1,49 @@
+package io.getquill.norm
+
+import test.Spec
+import io.getquill.ast._
+
+class BetaReductionSpec extends Spec {
+
+  "simplifies the ast by applying functons" - {
+    "tuple field" in {
+      val ast: Ast = Property(Tuple(List(Ident("a"))), "_1")
+      BetaReduction(ast) mustEqual Ident("a")
+    }
+    "function apply" in {
+      val function = Function(List(Ident("a")), Ident("a"))
+      val ast: Ast = FunctionApply(function, List(Ident("b")))
+      BetaReduction(ast) mustEqual Ident("b")
+    }
+  }
+
+  "replaces identifiers by actuals" - {
+    "ident" in {
+      val ast: Ast = Ident("a")
+      BetaReduction(ast, Ident("a") -> Ident("a'")) mustEqual
+        Ident("a'")
+    }
+    "avoids replacing idents of an outer scope" - {
+      "function" in {
+        val ast: Ast = Function(List(Ident("a")), Ident("a"))
+        BetaReduction(ast, Ident("a") -> Ident("a'")) mustEqual
+          Function(List(Ident("a")), Ident("a"))
+      }
+      "filter" in {
+        val ast: Ast = Filter(Ident("a"), Ident("b"), Ident("b"))
+        BetaReduction(ast, Ident("b") -> Ident("b'")) mustEqual
+          Filter(Ident("a"), Ident("b"), Ident("b"))
+      }
+      "map" in {
+        val ast: Ast = Map(Ident("a"), Ident("b"), Ident("b"))
+        BetaReduction(ast, Ident("b") -> Ident("b'")) mustEqual
+          Map(Ident("a"), Ident("b"), Ident("b"))
+      }
+      "flatMap" in {
+        val ast: Ast = FlatMap(Ident("a"), Ident("b"), Ident("b"))
+        BetaReduction(ast, Ident("b") -> Ident("b'")) mustEqual
+          FlatMap(Ident("a"), Ident("b"), Ident("b"))
+      }
+    }
+  }
+}
