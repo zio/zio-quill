d34ec648330e9d4d9357036ae229dbecae589c01
fwbrasil <fwbrasil@gmail.com>
2015-08-30 20:44:31 +0200

    test coverage

--- quill-core/src/test/scala/io/getquill/norm/capture/AvoidAliasConflictSpec.scala
+++ quill-core/src/test/scala/io/getquill/norm/capture/AvoidAliasConflictSpec.scala
@@ -8,17 +8,17 @@ class AvoidAliasConflictSpec extends Spec {
 
   "renames alias to avoid conflict between queryables during normalization" in {
     val q = quote {
-      qr1.filter(u => u.s == "s1").flatMap(u => qr2.flatMap(u => qr3.filter(u => u.s == "s1").map(u => u.s)))
+      qr1.filter(u => u.s == "s1").flatMap(u => qr2.flatMap(u => qr3.sortBy(u => u.s).filter(u => u.s == "s1").map(u => u.s)))
     }
     val n = quote {
-      qr1.filter(u => u.s == "s1").flatMap(u => qr2.flatMap(u1 => qr3.filter(u2 => u2.s == "s1").map(u => u.s)))
+      qr1.filter(u => u.s == "s1").flatMap(u => qr2.flatMap(u1 => qr3.sortBy(u2 => u2.s).filter(u => u.s == "s1").map(u => u.s)))
     }
     AvoidAliasConflict(q.ast) mustEqual n.ast
   }
 
   "doesn't change the query if it doesn't have conflicts" in {
     val q = quote {
-      qr1.flatMap(a => qr2.filter(b => b.s == "s1")).flatMap(c => qr3.map(d => d.s))
+      qr1.flatMap(a => qr2.sortBy(b => b.s).filter(c => c.s == "s1")).flatMap(d => qr3.map(e => e.s))
     }
     AvoidAliasConflict(q.ast) mustEqual q.ast
   }
--- quill-core/src/test/scala/io/getquill/norm/capture/DealiasSpec.scala
+++ quill-core/src/test/scala/io/getquill/norm/capture/DealiasSpec.scala
@@ -7,41 +7,81 @@ import language.reflectiveCalls
 class DealiasSpec extends Spec {
 
   "ensures that each entity is referenced by the same alias" - {
-    "filter.map" in {
-      val q = quote {
-        qr1.filter(a => a.s == "s1").map(b => b.s)
+    "filter" - {
+      "filter.map" in {
+        val q = quote {
+          qr1.filter(a => a.s == "s1").map(b => b.s)
+        }
+        val n = quote {
+          qr1.filter(a => a.s == "s1").map(a => a.s)
+        }
+        Dealias(q.ast) mustEqual n.ast
       }
-      val n = quote {
-        qr1.filter(a => a.s == "s1").map(a => a.s)
+      "filter.flatMap" in {
+        val q = quote {
+          qr1.filter(a => a.s == "s1").flatMap(b => qr2.filter(c => c == b))
+        }
+        val n = quote {
+          qr1.filter(a => a.s == "s1").flatMap(a => qr2.filter(c => c == a))
+        }
+        Dealias(q.ast) mustEqual n.ast
       }
-      Dealias(q.ast) mustEqual n.ast
+      "filter.filter" in {
+        val q = quote {
+          qr1.filter(a => a.s == "s1").filter(b => b.s == "s2")
+        }
+        val n = quote {
+          qr1.filter(a => a.s == "s1").filter(a => a.s == "s2")
+        }
+        Dealias(q.ast) mustEqual n.ast
+      }
+      "filter.sortBy" in {
+        val q = quote {
+          qr1.filter(a => a.s == "s1").sortBy(b => b.s)
+        }
+        val n = quote {
+          qr1.filter(a => a.s == "s1").sortBy(a => a.s)
+        }
+        Dealias(q.ast) mustEqual n.ast
+      }
     }
-    "filter.flatMap" in {
-      val q = quote {
-        qr1.filter(a => a.s == "s1").flatMap(b => qr2.filter(c => c == b))
+    "sortBy" - {
+      "sortBy.map" in {
+        val q = quote {
+          qr1.sortBy(a => a.s).map(b => b.s)
+        }
+        val n = quote {
+          qr1.sortBy(a => a.s).map(a => a.s)
+        }
+        Dealias(q.ast) mustEqual n.ast
       }
-      val n = quote {
-        qr1.filter(a => a.s == "s1").flatMap(a => qr2.filter(c => c == a))
+      "sortBy.flatMap" in {
+        val q = quote {
+          qr1.sortBy(a => a.s).flatMap(b => qr2.sortBy(c => c == b))
+        }
+        val n = quote {
+          qr1.sortBy(a => a.s).flatMap(a => qr2.sortBy(c => c == a))
+        }
+        Dealias(q.ast) mustEqual n.ast
       }
-      Dealias(q.ast) mustEqual n.ast
-    }
-    "filter.filter" in {
-      val q = quote {
-        qr1.filter(a => a.s == "s1").filter(b => b.s == "s2")
+      "sortBy.filter" in {
+        val q = quote {
+          qr1.sortBy(a => a.s).filter(b => b.s == "s2")
+        }
+        val n = quote {
+          qr1.sortBy(a => a.s).filter(a => a.s == "s2")
+        }
+        Dealias(q.ast) mustEqual n.ast
       }
-      val n = quote {
-        qr1.filter(a => a.s == "s1").filter(a => a.s == "s2")
+      "sortBy.sortBy" in {
+        val q = quote {
+          qr1.sortBy(a => a.s).sortBy(b => b.s)
+        }
+        val n = quote {
+          qr1.sortBy(a => a.s).sortBy(a => a.s)
+        }
+        Dealias(q.ast) mustEqual n.ast
       }
-      Dealias(q.ast) mustEqual n.ast
-    }
-    "filter.sortBy" in {
-      val q = quote {
-        qr1.filter(a => a.s == "s1").sortBy(b => b.s)
-      }
-      val n = quote {
-        qr1.filter(a => a.s == "s1").sortBy(a => a.s)
-      }
-      Dealias(q.ast) mustEqual n.ast
     }
   }
 }
