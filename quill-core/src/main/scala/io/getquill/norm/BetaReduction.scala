85b38ac0a88155c7e19fb26cdee174fadd032791
fwbrasil <fwbrasil@gmail.com>
2015-09-01 17:14:55 +0200

    refactor beta reduction

--- quill-core/src/main/scala/io/getquill/norm/BetaReduction.scala
+++ quill-core/src/main/scala/io/getquill/norm/BetaReduction.scala
@@ -11,65 +11,48 @@ import io.getquill.ast.Map
 import io.getquill.ast.Property
 import io.getquill.ast.Query
 import io.getquill.ast.SortBy
-import io.getquill.ast.StatefulTransformer
 import io.getquill.ast.Tuple
 import io.getquill.ast.Reverse
+import io.getquill.ast.StatelessTransformer
 
-case class BetaReduction(state: collection.Map[Ident, Ast])
-    extends StatefulTransformer[collection.Map[Ident, Ast]] {
+case class BetaReduction(map: collection.Map[Ident, Ast])
+    extends StatelessTransformer {
 
   override def apply(ast: Ast) =
     ast match {
       case Property(Tuple(values), name) =>
         apply(values(name.drop(1).toInt - 1))
       case FunctionApply(Function(params, body), values) =>
-        BetaReduction(state ++ params.zip(values)).apply(body) match {
-          case (body, t) => apply(body)
-        }
+        apply(BetaReduction(map ++ params.zip(values)).apply(body))
       case ident: Ident =>
-        (state.getOrElse(ident, ident), this)
+        map.getOrElse(ident, ident)
       case Function(params, body) =>
-        val (bodyt, t) = BetaReduction(state -- params)(body)
-        (Function(params, bodyt), this)
+        Function(params, BetaReduction(map -- params)(body))
       case other =>
         super.apply(other)
     }
 
   override def apply(query: Query) =
     query match {
-      case t: Entity =>
-        (t, this)
+      case t: Entity => t
       case Filter(a, b, c) =>
-        val (ar, art) = apply(a)
-        val (cr, crt) = BetaReduction(state - b)(c)
-        (Filter(ar, b, cr), this)
+        Filter(apply(a), b, BetaReduction(map - b)(c))
       case Map(a, b, c) =>
-        val (ar, art) = apply(a)
-        val (cr, crt) = BetaReduction(state - b)(c)
-        (Map(ar, b, cr), this)
+        Map(apply(a), b, BetaReduction(map - b)(c))
       case FlatMap(a, b, c) =>
-        val (ar, art) = apply(a)
-        val (cr, crt) = BetaReduction(state - b)(c)
-        (FlatMap(ar, b, cr), this)
+        FlatMap(apply(a), b, BetaReduction(map - b)(c))
       case SortBy(a, b, c) =>
-        val (ar, art) = apply(a)
-        val (cr, crt) = BetaReduction(state - b)(c)
-        (SortBy(ar, b, cr), this)
+        SortBy(apply(a), b, BetaReduction(map - b)(c))
       case Reverse(a) =>
-        val (ar, art) = apply(a)
-        (Reverse(ar), this)
+        Reverse(apply(a))
     }
 }
 
 object BetaReduction {
 
   def apply(ast: Ast, t: (Ident, Ast)*): Ast =
-    BetaReduction(t.toMap)(ast) match {
-      case (ast, _) => ast
-    }
+    BetaReduction(t.toMap)(ast)
 
   def apply(query: Query, t: (Ident, Ast)*): Query =
-    BetaReduction(t.toMap)(query) match {
-      case (ast, _) => ast
-    }
+    BetaReduction(t.toMap)(query)
 }
