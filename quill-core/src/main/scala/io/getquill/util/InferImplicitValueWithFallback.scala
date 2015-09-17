b9e995f900b2b281804d1295a99a6d02afd024f3
fwbrasil <fwbrasil@gmail.com>
2015-09-17 10:08:33 +0200

    wip - use scala implicit resolution to infer decoders/encoders from the source instance

--- quill-core/src/main/scala/io/getquill/util/InferImplicitValueWithFallback.scala
+++ quill-core/src/main/scala/io/getquill/util/InferImplicitValueWithFallback.scala
@@ -7,24 +7,17 @@ object InferImplicitValueWithFallback {
   def apply(c: Context)(tpe: c.Type, fallbackTree: c.Tree) = {
     import c.universe._
 
-    def fallback = {
+    def fallback =
+      c.typecheck(
+        q"""{
+          import $fallbackTree._
+          _root_.scala.Predef.implicitly[$tpe]
+        }""",
+        silent = true)
 
-      def fallbackImplicits =
-        fallbackTree.tpe.members.collect {
-          case m: MethodSymbol if (m.isImplicit && m.typeSignature.resultType.dealias <:< tpe) =>
-            m
-        }
-
-      fallbackImplicits.headOption.map { m =>
-        q"$fallbackTree.${m.name}"
-      }
+    c.inferImplicitValue(tpe).orElse(fallback) match {
+      case EmptyTree => None
+      case value     => Some(value)
     }
-
-    def inferImplicitValue(tpe: Type) =
-      c.inferImplicitValue(tpe) match {
-        case EmptyTree => None
-        case value     => Some(value)
-      }
-    inferImplicitValue(tpe).orElse(fallback)
   }
 }
