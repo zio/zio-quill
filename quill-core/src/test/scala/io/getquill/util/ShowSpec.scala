e2b3976c5464cfc17330f7f0406690ae984a53f6
fwbrasil <fwbrasil@gmail.com>
2015-11-14 18:33:08 -0800

    show spec

---
+++ quill-core/src/test/scala/io/getquill/util/ShowSpec.scala
@@ -0,0 +1,20 @@
+package io.getquill.util
+
+import io.getquill.Spec
+
+class ShowSpec extends Spec {
+  
+  import Show._
+  
+  "given a Show implicit, provides an implicit class with the show method" in {
+    implicit val show = new Show[Int] {
+      def show(v: Int) = v.toString
+    }
+    1.show mustEqual "1"
+  }
+  
+  "provides a factory method that receives a function" in {
+    implicit val show = Show[Int](_.toString)
+    1.show mustEqual "1"
+  }
+}
