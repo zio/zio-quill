13b2c275bb406cc76747ca9d675284c7034a24d3
fwbrasil <fwbrasil@gmail.com>
2015-12-20 02:42:26 -0800

    trying to fix the ssh key

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 if [[ $TRAVIS_TAG -eq 'release' ]]
 then
-	eval "$(ssh-agent -s)"
-	chmod 600 local.deploy_key.pem
-	echo $ENCRYPTION_PASSWORD | ssh-add -p local.deploy_key.pem
+	./add_ssh_key
 	git config --global user.name "Quill CI"
 	git config --global user.email "quillci@getquill.io"
 	git remote add origin git@github.com:getquill/quill.git 
-	sbt release with-defaults
+	sbt clean release with-defaults
 	git push --delete origin release
 else
-	sbt publish
+	sbt clean coverage test tut coverageAggregate
 fi
