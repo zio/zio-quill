2b54e8c5b8747b1d6bae1784bf718c7c78b5523e
fwbrasil <fwbrasil@gmail.com>
2015-12-20 02:51:29 -0800

    fix git remote

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-if [[ $TRAVIS_TAG -eq 'release' ]]
+if [[ $TRAVIS_TAG =~ 'release' ]]
 then
 	eval "$(ssh-agent -s)"
 	chmod 600 local.deploy_key.pem
 	ssh-add local.deploy_key.pem
 	git config --global user.name "Quill CI"
 	git config --global user.email "quillci@getquill.io"
-	git remote add origin git@github.com:getquill/quill.git 
+	git remote set-url origin git@github.com:getquill/quill.git 
 	sbt clean release with-defaults
-	git push --delete origin release
+	git push --delete deploy release
 else
 	sbt clean coverage test tut coverageAggregate
 fi
