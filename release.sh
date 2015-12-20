1d504caed930ea6cb0ae4ccd1b328a5f8c45c496
fwbrasil <fwbrasil@gmail.com>
2015-12-20 02:10:51 -0800

    use the quillci user to push code

--- release.sh
+++ release.sh
@@ -3,7 +3,9 @@ if [[ $TRAVIS_TAG =~ 'release' ]]
 then
 	eval "$(ssh-agent -s)"
 	chmod 600 local.deploy_key.pem
-	ssh-add local.deploy_key.pem
+	echo $ENCRYPTION_PASSWORD | ssh-add local.deploy_key.pem
+	git config --global user.name "Quill CI"
+	git config --global user.email "quillci@getquill.io"
 	git remote add origin git@github.com:getquill/quill.git 
 	sbt release with-defaults
 	git push --delete origin release
