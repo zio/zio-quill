014ef519b734b451d2a44f369dc816f1d0c50374
fwbrasil <fwbrasil@gmail.com>
2015-12-20 03:25:38 -0800

    release from master

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ then
 	ssh-add local.deploy_key.pem
 	git config --global user.name "Quill CI"
 	git config --global user.email "quillci@getquill.io"
+	git fetch
 	git remote set-url origin git@github.com:getquill/quill.git
 	sbt clean release with-defaults
 	git push --delete deploy release
