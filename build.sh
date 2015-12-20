c3b571952db62916fbdf1c2032d5d8a140095a28
fwbrasil <fwbrasil@gmail.com>
2015-12-20 10:40:03 -0800

    trying to fix "ref HEAD is not a symbolic ref"

--- build.sh
+++ build.sh
@@ -6,9 +6,10 @@ then
 	ssh-add local.deploy_key.pem
 	git config --global user.name "Quill CI"
 	git config --global user.email "quillci@getquill.io"
-	git fetch
 	git remote set-url origin git@github.com:getquill/quill.git
-	sbt clean release with-defaults
+	git fetch --unshallow
+	git symbolic-ref HEAD
+	sbt tut clean release with-defaults
 	git push --delete deploy release
 else
 	sbt clean coverage test tut coverageAggregate
