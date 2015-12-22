af309b16f52ae75f9c53e5967af4ef01d886dad6
Julius de Bruijn <julius.bruijn@sponsorpay.com>
2015-12-22 18:00:33 +0100

    Add finagle mysql pool configuration
    This allows an optional configuration for finagle's connection pool.

--- quill-finagle-mysql/src/main/scala/io/getquill/source/finagle/mysql/FinagleMysqlClient.scala
+++ quill-finagle-mysql/src/main/scala/io/getquill/source/finagle/mysql/FinagleMysqlClient.scala
@@ -1,6 +1,9 @@
 package io.getquill.source.finagle.mysql
 
+import com.twitter.finagle.client.DefaultPool
 import com.twitter.finagle.exp.Mysql
+import com.twitter.util.Try
+import com.twitter.conversions.time._
 import com.typesafe.config.Config
 
 object FinagleMysqlClient {
@@ -10,10 +13,20 @@ object FinagleMysqlClient {
     val password = config.getString("password")
     val database = config.getString("database")
     val dest = config.getString("dest")
+    val lowWatermark = Try(config.getInt("pool.watermark.low")).getOrElse(0)
+    val highWatermark = Try(config.getInt("pool.watermark.high")).getOrElse(10)
+    val idleTime = Try(config.getInt("pool.idleTime")).getOrElse(5)
+    val bufferSize = Try(config.getInt("pool.bufferSize")).getOrElse(0)
+    val maxWaiters = Try(config.getInt("pool.maxWaiters")).getOrElse(Int.MaxValue)
 
     Mysql.client
       .withCredentials(user, password)
       .withDatabase(database)
+      .configured(DefaultPool.Param(
+        low = lowWatermark, high = highWatermark,
+        idleTime = idleTime.seconds,
+        bufferSize = bufferSize,
+        maxWaiters = maxWaiters))
       .newRichClient(dest)
   }
 }
