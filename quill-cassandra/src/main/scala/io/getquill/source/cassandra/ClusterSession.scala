86cd866fc41c29eaaf3bc8e3e113dc75a42ab9bf
fwbrasil <fwbrasil@gmail.com>
2016-01-16 22:03:12 -0800

    fix #99 - quill-cassandra: Expose all config options available in Cluster.builder

--- quill-cassandra/src/main/scala/io/getquill/source/cassandra/ClusterSession.scala
+++ quill-cassandra/src/main/scala/io/getquill/source/cassandra/ClusterSession.scala
@@ -1,26 +1,10 @@
 package io.getquill.source.cassandra
 
-import com.datastax.driver.core.Cluster
 import com.typesafe.config.Config
-import com.datastax.driver.core.QueryOptions
-import com.datastax.driver.core.ConsistencyLevel
 
 object ClusterSession {
 
-  def apply(config: Config) = {
-    var builder = Cluster.builder
-
-    builder = builder.addContactPoints(config.getString("contactPoints"))
-
-    if (config.hasPath("port"))
-      builder = builder.withPort(config.getInt("port"))
-
-    if (config.hasPath("user") && config.hasPath("password"))
-      builder = builder.withCredentials(config.getString("user"), config.getString("password"))
-
-    if (config.hasPath("ssl") && config.getBoolean("ssl"))
-      builder = builder.withSSL
-
-    builder.withQueryOptions((new QueryOptions).setConsistencyLevel(ConsistencyLevel.ALL)).build.connect(config.getString("keyspace"))
-  }
+  def apply(cfg: Config) =
+    ClusterBuilder(cfg.getConfig("session")).build
+      .connect(cfg.getString("keyspace"))
 }
