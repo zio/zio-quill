f06cfe2d050e09d4f36c0e0fdedd8607c1ac9130
Luis Sanchez <luis.sanchez@williamhill.com>
2016-01-24 19:38:52 +0000

    * quill-core: add tests to ensure the config prefix is right for different use cases

--- quill-core/src/test/scala/io/getquill/source/ResolveSourceMacroSpec.scala
+++ quill-core/src/test/scala/io/getquill/source/ResolveSourceMacroSpec.scala
@@ -14,4 +14,24 @@ class ResolveSourceMacroSpec extends Spec {
     case class Fail()
     "io.getquill.source.mirror.mirrorSource.run(query[Fail].delete)" must compile
   }
+
+  "identifies config prefix when source object used directly" in {
+    object ObjectSource extends MirrorSourceTemplate
+    "ObjectSource.run(qr1.delete)" must compile
+    ObjectSource.configPrefix must equal("ObjectSource2")
+  }
+
+  "identifies config prefix when source object is assigned to a variable" in {
+    object VariableSource extends MirrorSourceTemplate
+    val db = VariableSource
+    "db.run(qr1.delete)" must compile
+    db.configPrefix must equal("VariableSource2")
+  }
+
+  "identifies config prefix when source is a class" in {
+    class ClassSource() extends MirrorSourceTemplate
+    val db = new ClassSource()
+    "db.run(qr1.delete)" must compile
+    db.configPrefix must equal("ClassSource1")
+  }
 }
