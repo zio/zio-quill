a24f7ee1f0965874489a46bd6b141f6c6f15b40c
jilen <jilen.zhang@gmail.com>
2016-01-25 17:11:13 +0800

    add mysql quote

--- quill-core/src/main/scala/io/getquill/naming/NamingStrategy.scala
+++ quill-core/src/main/scala/io/getquill/naming/NamingStrategy.scala
@@ -79,3 +79,12 @@ trait CamelCase extends NamingStrategy {
     }
 }
 object CamelCase extends CamelCase
+
+trait MysqlQuote extends NamingStrategy {
+  override def table(s: String) = quote(s)
+  override def column(s: String) = quote(s)
+  override def default(s: String) = s
+  private def quote(s: String) = s"`$s`"
+}
+
+object MysqlQuote extends MysqlQuote
