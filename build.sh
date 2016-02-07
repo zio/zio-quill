89abd1ac9905ef260cc37a9f66b991d21552cbd5
Gustavo Amigo <gustavo.amigo@gmail.com>
2016-02-07 18:54:26 -0200

    Travis CI with Docker Compose

--- build.sh
+++ build.sh
@@ -20,8 +20,8 @@ then
 		sbt tut 'release with-defaults'
 		git push --delete origin release
 	else
-		sbt clean coverage test tut coverageAggregate && bash <(curl -s https://codecov.io/bash) && sbt clean publish
+		docker-compose run sbt sbt clean coverage test tut coverageAggregate && bash <(curl -s https://codecov.io/bash) && docker-compose run sbt sbt clean publish
 	fi
 else
-	sbt clean coverage test tut coverageAggregate && bash <(curl -s https://codecov.io/bash)
+	ocker-compose run sbt clean coverage test tut coverageAggregate && bash <(curl -s https://codecov.io/bash)
 fi
