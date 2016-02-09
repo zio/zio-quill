7c500d32ef330f567b3f416b6e4eebfe78a2ee78
godenji <admin@ushr.com>
2016-02-09 18:47:36 -0500

    revert to upstream master, implemented type safe join parser; @see MergeSecondaryJoin

--- quill-core/src/main/scala/io/getquill/util/Show.scala
+++ quill-core/src/main/scala/io/getquill/util/Show.scala
@@ -16,26 +16,6 @@ object Show {
   }
 
   implicit def listShow[T](implicit shower: Show[T]) = Show[List[T]] {
-    case list if !list.exists(_.toString.contains("JoinSource(")) =>
-	    list.map(_.show).mkString(", ")
-    case joinParts =>
-    	val xs = joinParts.map(_.show)
-    	//xs foreach println
-    	val parts = 
-    		if(xs.size > 1) {
-    			// head in format: table x1
-    			// tail in format: JOIN table t ON t.id = a.id
-    			val tail = xs.tail. // strip leading table(s)
-	    			flatMap(_.split(joinTypes).toList).
-						map(_.trim)
-					tail foreach println
-						
-	    		xs.head :: 
-	    		xs.tail. // strip leading table(s)
-	    			flatMap(_.split(joinTypes).toList.tail).
-						map(_.trim)
-	    	} else xs
-    	parts.mkString(" ")
+    case list => list.map(_.show).mkString(", ")
   }
-  private val joinTypes = "INNER|LEFT|RIGHT|FULL"
 }
