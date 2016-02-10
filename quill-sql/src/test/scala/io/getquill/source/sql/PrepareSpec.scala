a9890d9bf492c90c34286dc71d49454b9bc0dfaa
godenji <admin@ushr.com>
2016-02-10 08:27:46 -0500

    lift secondary join AST flatMap/map calls to tuple join; i.e. nested joins a la Join(INNER, Join(TableA, TableB, on ..), TableC, on ..)

--- quill-sql/src/test/scala/io/getquill/source/sql/PrepareSpec.scala
+++ quill-sql/src/test/scala/io/getquill/source/sql/PrepareSpec.scala
@@ -19,4 +19,18 @@ class PrepareSpec extends Spec {
     mirrorSource.run(q).sql mustEqual
       "SELECT t._1, t1._2 FROM TestEntity a LEFT JOIN (SELECT t.i _1 FROM TestEntity2 t) t ON a.i > t._1, TestEntity a1 LEFT JOIN (SELECT t1.l _2 FROM TestEntity2 t1) t1 ON a1.l < t1._2"
   }
+  
+  "mirror sql joins" in {
+    val q = quote{
+      for{
+        (a,b)<- qr1 join qr2 on(_.i == _.i)
+           c <- qr1 join(_.i == a.i)
+           d <- qr2 join(_.i == b.i)
+           e <- qr3 join(_.i == c.i)
+      } yield (a,b,c,d,e)
+    }
+    mirrorSource.run(q).sql mustEqual
+      "SELECT x3.s, x3.i, x3.l, x3.o, x4.s, x4.i, x4.l, x4.o, x5.s, x5.i, x5.l, x5.o, x6.s, x6.i, x6.l, x6.o, x7.s, x7.i, x7.l, x7.o FROM TestEntity x3 INNER JOIN TestEntity2 x4 ON x3.i = x4.i INNER JOIN TestEntity x5 ON x5.i = x3.i INNER JOIN TestEntity2 x6 ON x6.i = x4.i INNER JOIN TestEntity3 x7 ON x7.i = x5.i"
+  }
+  	
 }
