a6c1c04ef271df13473087381aee9eb87b7aaf1d
jilen <jilen.zhang@gmail.com>
2016-02-25 11:14:18 +0800

    add test for async sources

---
+++ quill-async/src/test/scala/io/getquill/sources/async/mysql/MysqlAsyncSourceSpec.scala
@@ -0,0 +1,18 @@
+package io.getquill.sources.async.mysql
+
+import io.getquill._
+import scala.concurrent.{ Await, Future }
+import scala.concurrent.duration.Duration
+import scala.concurrent.ExecutionContext.Implicits.{ global => ec }
+
+class MysqlAsyncSourceSpec extends Spec {
+
+  def await[T](f: Future[T]) = Await.result(f, Duration.Inf)
+
+  "run non-batched action" - {
+    val insert = quote { (i: Int) =>
+      qr1.insert(_.i -> i)
+    }
+    await(testMysqlDB.run(insert)(1)).rowsAffected mustEqual (1)
+  }
+}
