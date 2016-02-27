89fea5f780e769cba34eab729a81f9ed43f13bb8
Gustavo Amigo <gustavo.amigo@gmail.com>
2016-02-27 03:13:47 -0300

    Faster and more reliable build * Migrated Docker image to Alpine for faster builds * Verifies if databases are ready before running script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
-sleep 30
+#!/bin/sh
+
+echo "Waiting for Mysql"
+until mysql -u root -proot -h mysql -e "SELECT 1" &> /dev/null
+do
+  printf "."
+  sleep 1
+done
+echo -e "\nMysql ready"
 
 mysql -u root -proot -h mysql -e "ALTER USER 'root'@'%' IDENTIFIED BY ''"
 mysql -u root -h mysql quill_test < quill-sql/src/test/sql/mysql-schema.sql
@@ -7,8 +14,25 @@ mysql -u root -h mysql -e "CREATE USER 'finagle'@'%' IDENTIFIED BY 'finagle';"
 mysql -u root -h mysql -e "GRANT ALL PRIVILEGES ON * . * TO 'finagle'@'%';"
 mysql -u root -h mysql -e "FLUSH PRIVILEGES;"
 
-createdb -h postgres quill_test -U postgres
+echo "Waiting for Postgres"
+until psql -h postgres -U postgres -c "SELECT 1" &> /dev/null
+do
+  printf "."
+  sleep 1
+done
+echo -e "\nPostgres ready"
+
+psql -h postgres -U postgres -c "CREATE DATABASE quill_test"
 psql -h postgres -U postgres -d quill_test -a -f quill-sql/src/test/sql/postgres-schema.sql
+
+echo "Waiting for Cassandra"
+if ! nc -z cassandra 9042; then
+    while ! nc -z cassandra 9042; do
+       printf "."
+       sleep 1
+    done
+    echo -e "\nCassandra ready"
+fi
 
 echo "CREATE KEYSPACE quill_test WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};" > /tmp/create-keyspace.cql
 cqlsh cassandra -f /tmp/create-keyspace.cql
