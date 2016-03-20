3d1835900f5bea92cbf59b8e82febcdb657dce54
fwbrasil <fwbrasil@gmail.com>
2016-03-20 15:35:19 -0700

    publish branch snapshots automatically

--- build/build.sh
+++ build/build.sh
@@ -2,14 +2,14 @@
 chown root ~/.ssh/config
 chmod 644 ~/.ssh/config
 
-if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]]
+if [[ $TRAVIS_PULL_REQUEST == "false" ]]
 then
 	openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in ./build/secring.gpg.enc -out local.secring.gpg -d
 	openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in ./build/pubring.gpg.enc -out local.pubring.gpg -d
 	openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in ./build/credentials.sbt.enc -out local.credentials.sbt -d
 	openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in ./build/deploy_key.pem.enc -out local.deploy_key.pem -d
 
-	if [[ $(cat version.sbt) != *"SNAPSHOT"* ]]
+	if [[ $TRAVIS_BRANCH == "master" && $(cat version.sbt) != *"SNAPSHOT"* ]]
 	then
 		eval "$(ssh-agent -s)"
 		chmod 600 local.deploy_key.pem
@@ -22,8 +22,13 @@ then
 		git reset --hard origin/master
 		sbt tut 'release with-defaults'
 		git push --delete origin release
-	else
+	elif [[ $TRAVIS_BRANCH == "master" ]]
+	then
 		sbt clean coverage test tut coverageAggregate release-vcs-checks && sbt coverageOff publish
+	else
+		sbt clean coverage test tut coverageAggregate release-vcs-checks
+		echo "version in ThisBuild := \"$TRAVIS_BRANCH-SNAPSHOT\"" > version.sbt
+		sbt coverageOff publish
 	fi
 else
 	sbt clean coverage test tut coverageAggregate release-vcs-checks
