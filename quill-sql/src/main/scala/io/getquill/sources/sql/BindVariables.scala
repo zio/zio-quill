juliano/quill

Allow Free Variables anywhere

quill-sql/src/main/scala/io/getquill/sources/sql/BindVariables.scala
@@ -13,6 +13,9 @@ private[sources] case class BindVariables(state: (List[Ident], List[Ident]))
           case true  => (Ident("?"), BindVariables((vars, bindings :+ i)))
           case false => (i, this)
         }
+      case (b: Binding) =>
+        val (vars, bindings) = state
+        (Ident("?"), BindVariables((vars, bindings :+ Ident(b.key))))
       case other => super.apply(ast)
     }
 

zio/zio-quill

quill-sql/src/main/scala/io/getquill/sources/sql/BindVariables.scala
@@ -13,6 +13,9 @@ private[sources] case class BindVariables(state: (List[Ident], List[Ident]))
           case true  => (Ident("?"), BindVariables((vars, bindings :+ i)))
           case false => (i, this)
         }
+      case (b: RuntimeBinding) =>
+        val (vars, bindings) = state
+        (Ident("?"), BindVariables((vars, bindings :+ Ident(b.key))))
       case other => super.apply(ast)
     }
 