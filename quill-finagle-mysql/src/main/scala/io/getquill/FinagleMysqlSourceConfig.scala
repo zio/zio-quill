98657e481ca8a4e8ebca17b5265f8cb4877d40ac
thirstycrow <thirstycrow@qq.com>
2016-06-07 14:04:33 +0800

    add methods to construct FinagleMysqlSourceConfig's

--- quill-finagle-mysql/src/main/scala/io/getquill/FinagleMysqlSourceConfig.scala
+++ quill-finagle-mysql/src/main/scala/io/getquill/FinagleMysqlSourceConfig.scala
@@ -2,12 +2,28 @@ package io.getquill
 
 import com.twitter.finagle.client.DefaultPool
 import com.twitter.finagle.exp.Mysql
+import com.twitter.finagle.exp.mysql.Transactions
+import com.twitter.finagle.exp.mysql.Client
 import com.twitter.util.Try
 import com.twitter.conversions.time._
 import io.getquill.sources.SourceConfig
 import io.getquill.naming.NamingStrategy
 import java.util.TimeZone
 import io.getquill.sources.finagle.mysql.FinagleMysqlSource
+
+object FinagleMysqlSourceConfig {
+
+  def apply[N <: NamingStrategy](
+    clnt: Client with Transactions,
+    dataTimeZone: TimeZone = TimeZone.getDefault) = {
+
+    new FinagleMysqlSourceConfig[N]("") {
+      override def client = clnt
+    }
+  }
+
+  def apply[N <: NamingStrategy](name: String) = new FinagleMysqlSourceConfig[N](name)
+}
 
 class FinagleMysqlSourceConfig[N <: NamingStrategy](val name: String) extends SourceConfig[FinagleMysqlSource[N]] {
 
