94aae4cda54bd5db647de3a3a9119fc121c4bc93
jilen <jilen.zhang@gmail.com>
2016-07-11 15:24:34 +0800

    fix empty set query

--- quill-sql/src/main/scala/io/getquill/sources/sql/SqlBindedStatementBuilder.scala
+++ quill-sql/src/main/scala/io/getquill/sources/sql/SqlBindedStatementBuilder.scala
@@ -3,5 +3,5 @@ package io.getquill.sources.sql
 import io.getquill.sources.BindedStatementBuilder
 
 class SqlBindedStatementBuilder[S] extends BindedStatementBuilder[S] {
-  override def emptySet: String = "SELECT NULL LIMIT 0"
+  override def emptySet: String = "SELECT 0 FROM (SELECT 0) AS TBL WHERE FALSE"
 }
