b2e074ab1404a1ad5cb8e2310548899f2f62c60c
Flavio W. Brasil <fwbrasil@users.noreply.github.com>
2016-11-04 13:16:29 -0700

    avoid merging filter condition into a groupBy.map (#618)

--- quill-core/src/main/scala/io/getquill/norm/AttachToEntity.scala
+++ quill-core/src/main/scala/io/getquill/norm/AttachToEntity.scala
@@ -13,17 +13,16 @@ object AttachToEntity {
       case Filter(a: Entity, b, c) => Filter(f(a, b), b, c)
       case SortBy(a: Entity, b, c, d) => SortBy(f(a, b), b, c, d)
 
+      case Map(_: GroupBy, _, _) | _: Union | _: UnionAll | _: Join | _: FlatJoin => f(q, alias.getOrElse(Ident("x")))
+
       case Map(a: Query, b, c) => Map(apply(f, Some(b))(a), b, c)
       case FlatMap(a: Query, b, c) => FlatMap(apply(f, Some(b))(a), b, c)
       case Filter(a: Query, b, c) => Filter(apply(f, Some(b))(a), b, c)
       case SortBy(a: Query, b, c, d) => SortBy(apply(f, Some(b))(a), b, c, d)
       case Take(a: Query, b) => Take(apply(f, alias)(a), b)
       case Drop(a: Query, b) => Drop(apply(f, alias)(a), b)
-      case GroupBy(a: Query, b, c) => GroupBy(apply(f, Some(b))(a), b, c)
       case Aggregation(op, a: Query) => Aggregation(op, apply(f, alias)(a))
       case Distinct(a: Query) => Distinct(apply(f, alias)(a))
-
-      case _: Union | _: UnionAll | _: Join | _: FlatJoin => f(q, alias.getOrElse(Ident("x")))
 
       case e: Entity => f(e, alias.getOrElse(Ident("x")))
 
