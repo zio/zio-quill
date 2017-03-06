2de816f5400282ff1e84ece8b3f6c9c006d0b3dd
karellen <karellen.kim@daumkakao.com>
2017-03-06 16:00:53 +0900

    Fix the issue occured by differences between DB and `finagle-mysql` client timezone.
    - Add assert code related with Date in FinagleMysqlContextConfig.
    - Add decoder for Date type of Mysql.
    - Add max prepare statement configuration.

--- quill-finagle-mysql/src/main/scala/io/getquill/FinagleMysqlContextConfig.scala
+++ quill-finagle-mysql/src/main/scala/io/getquill/FinagleMysqlContextConfig.scala
@@ -9,7 +9,8 @@ import com.twitter.util.TimeConversions._
 
 case class FinagleMysqlContextConfig(config: Config) {
 
-  def dateTimezone = TimeZone.getDefault
+  def injectionTimeZone = Try(TimeZone.getTimeZone(config.getString("timezone.injection"))).getOrElse(TimeZone.getDefault)
+  def extractionTimeZone = Try(TimeZone.getTimeZone(config.getString("timezone.extraction"))).getOrElse(TimeZone.getDefault)
   def user = config.getString("user")
   def password = Try(config.getString("password")).getOrElse(null)
   def database = config.getString("database")
@@ -19,11 +20,13 @@ case class FinagleMysqlContextConfig(config: Config) {
   def idleTime = Try(config.getInt("pool.idleTime")).getOrElse(5)
   def bufferSize = Try(config.getInt("pool.bufferSize")).getOrElse(0)
   def maxWaiters = Try(config.getInt("pool.maxWaiters")).getOrElse(Int.MaxValue)
+  def maxPrepareStatements = Try(config.getInt("maxPrepareStatements")).getOrElse(20)
 
   def client =
     Mysql.client
       .withCredentials(user, password)
       .withDatabase(database)
+      .withMaxConcurrentPrepareStatements(maxPrepareStatements)
       .configured(DefaultPool.Param(
         low = lowWatermark, high = highWatermark,
         idleTime = idleTime.seconds,
