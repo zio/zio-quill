7ad3d5789567dd6cf34f8269bc32d90323c8a4ab
Juliano Alves <von.juliano@gmail.com>
2017-05-19 21:41:45 +0100

    Fix msodbcql

--- build/setup.sh
+++ build/setup.sh
@@ -57,7 +57,7 @@ do
   printf "."
   sleep 1
 done
-echo "\nSql Server ready"
+echo -e "\nSql Server ready"
 
 sqlcmd -S sqlserver -U SA -P "QuillRocks!" -Q "CREATE DATABASE quill_test"
 sqlcmd -S sqlserver -U SA -P "QuillRocks!" -d quill_test -i quill-sql/src/test/sql/sqlserver-schema.sql
