0e46b28c73d17cd58d2bebe37dd5b48b7089b30b
Juliano Alves <von.juliano@gmail.com>
2017-05-26 14:50:23 +0100

    Fix test

--- quill-jdbc/src/test/scala/io/getquill/context/jdbc/sqlite/JdbcEncodingSpec.scala
+++ quill-jdbc/src/test/scala/io/getquill/context/jdbc/sqlite/JdbcEncodingSpec.scala
@@ -7,7 +7,7 @@ class JdbcEncodingSpec extends EncodingSpec {
   val context = testContext
   import testContext._
 
-  "encodes and decodes types" in pendingUntilFixed {
+  "encodes and decodes types" in {
     testContext.run(delete)
     testContext.run(liftQuery(insertValues).foreach(e => insert(e)))
     verify(testContext.run(query[EncodingTestEntity]))
