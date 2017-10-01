61ed89df6e4ec176da65dc1e99a7332abed38688
fwbrasil <fwbrasil@gmail.com>
2017-10-01 09:49:56 -0700

    fix build

--- quill-sql/src/test/scala/io/getquill/context/sql/idiom/VerifySqlQuerySpec.scala
+++ quill-sql/src/test/scala/io/getquill/context/sql/idiom/VerifySqlQuerySpec.scala
@@ -30,7 +30,7 @@ class VerifySqlQuerySpec extends Spec {
           case (a, b) => b.isDefined
         }
       }
-      VerifySqlQuery(SqlQuery(q.ast)).toString mustEqual 
+      VerifySqlQuery(SqlQuery(q.ast)).toString mustEqual
         "Some(The monad composition can't be expressed using applicative joins. Faulty expression: 'x01._2.isDefined'. Free variables: 'List(x01)'.)"
     }
 
