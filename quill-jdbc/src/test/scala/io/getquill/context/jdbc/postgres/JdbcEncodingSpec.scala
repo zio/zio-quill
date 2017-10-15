35e1dfd259b561f6a11cf34cfd2ac2e188f8c2f0
mentegy <mentegy@gmail.com>
2017-10-15 13:54:06 +0300

    Improve coverage for jdbc contexts

--- quill-jdbc/src/test/scala/io/getquill/context/jdbc/postgres/JdbcEncodingSpec.scala
+++ quill-jdbc/src/test/scala/io/getquill/context/jdbc/postgres/JdbcEncodingSpec.scala
@@ -1,5 +1,7 @@
 package io.getquill.context.jdbc.postgres
 
+import java.time.{ LocalDate, LocalDateTime }
+
 import io.getquill.context.sql.EncodingSpec
 
 class JdbcEncodingSpec extends EncodingSpec {
@@ -28,5 +30,20 @@ class JdbcEncodingSpec extends EncodingSpec {
     val (barCode :: Nil) = testContext.run(findBarCodeByUuid(uuid))
 
     verifyBarcode(barCode)
+  }
+
+  "LocalDateTime" in {
+    case class EncodingTestEntity(v11: LocalDateTime)
+    case class E(v11: Option[LocalDateTime])
+    val e = EncodingTestEntity(LocalDateTime.now())
+    val res: List[EncodingTestEntity] = performIO {
+      for {
+        _ <- testContext.runIO(query[EncodingTestEntity].delete)
+        _ <- testContext.runIO(query[EncodingTestEntity].insert(lift(e)))
+        _ <- testContext.runIO(querySchema[E]("EncodingTestEntity").insert(lift(E(None))))
+        r <- testContext.runIO(query[EncodingTestEntity])
+      } yield r
+    }
+    res must contain theSameElementsAs List(e, EncodingTestEntity(LocalDate.ofEpochDay(0).atStartOfDay()))
   }
 }
