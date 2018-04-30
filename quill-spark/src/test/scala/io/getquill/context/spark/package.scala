398d160a2456e710d426c2fb3f417bcb9c5b098b
fwbrasil <fwbrasil@gmail.com>
2018-04-30 15:38:34 -0700

    disable spark ui

--- quill-spark/src/test/scala/io/getquill/context/spark/package.scala
+++ quill-spark/src/test/scala/io/getquill/context/spark/package.scala
@@ -9,6 +9,7 @@ package object spark {
     SparkSession
       .builder()
       .config("spark.sql.shuffle.partitions", 2) // Default shuffle partitions is 200, too much for tests
+      .config("spark.ui.enabled", "false")
       .master("local[1]")
       .appName("spark test")
       .getOrCreate()
