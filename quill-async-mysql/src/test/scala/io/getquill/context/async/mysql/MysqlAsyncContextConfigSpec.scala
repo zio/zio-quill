33b98b1b0b9d78e5956a892ed54fa93bdc667f67
Andrew <m0hct3r@gmail.com>
2018-05-08 21:39:44 +0300

    tests fixes

--- quill-async-mysql/src/test/scala/io/getquill/context/async/mysql/MysqlAsyncContextConfigSpec.scala
+++ quill-async-mysql/src/test/scala/io/getquill/context/async/mysql/MysqlAsyncContextConfigSpec.scala
@@ -3,32 +3,63 @@ package io.getquill.context.async.mysql
 import java.nio.charset.Charset
 
 import com.typesafe.config.ConfigFactory
-import io.getquill.{ MysqlAsyncContextConfig, Spec }
+import io.getquill.{MysqlAsyncContextConfig, Spec}
 
 import scala.collection.JavaConverters._
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration._
 
 class MysqlAsyncContextConfigSpec extends Spec {
-  class AsyncContextConfigSpec extends Spec {
-    "parses url" in {
-      val c = ConfigFactory.parseMap(Map(
-        "url" -> "jdbc:postgresql://host:5233/db?user=p",
-        "pass" -> "pass",
-        "queryTimeout" -> "123",
-        "charset" -> "UTF-8",
-        "maximumMessageSize" -> "123",
-        "connectTimeout" -> "123"
-      ).asJava)
-      val conf = MysqlAsyncContextConfig(c)
 
-      conf.queryTimeout mustBe Some(Duration("123"))
-      conf.connectTimeout mustBe Some(Duration("123"))
-      conf.maximumMessageSize mustBe Some(123)
-      conf.charset mustBe Some(Charset.forName("UTF-8"))
-      conf.host mustBe Some("123")
-      conf.user mustBe Some("p")
-      conf.password mustBe Some("pass")
-    }
+  "parses url" in {
+
+    val c = ConfigFactory.parseMap(Map(
+      "url" -> "jdbc:postgresql://github.com:5233/db?user=p",
+      "pass" -> "pass",
+      "queryTimeout" -> "123 s",
+      "host" -> "github.com",
+      "port" -> "5233",
+      "charset" -> "UTF-8",
+      "user"-> "p",
+      "password" -> "pass",
+      "maximumMessageSize" -> "456",
+      "connectTimeout" -> "789 s"
+    ).asJava)
+    val conf = MysqlAsyncContextConfig(c)
+
+    conf.queryTimeout mustBe Some(123.seconds)
+    conf.connectTimeout mustBe Some(789.seconds)
+    conf.maximumMessageSize mustBe Some(456)
+    conf.charset mustBe Some(Charset.forName("UTF-8"))
+    conf.host mustBe Some("github.com")
+    conf.port mustBe Some(5233)
+    conf.user mustBe Some("p")
+    conf.password mustBe Some("pass")
+  }
+
+
+  "parses url and passes valid data to configuration" in {
+    val c = ConfigFactory.parseMap(Map(
+      "url" -> "jdbc:postgresql://github.com:5233/db?user=p",
+      "pass" -> "pass",
+      "queryTimeout" -> "123 s",
+      "host" -> "github.com",
+      "port" -> "5233",
+      "charset" -> "UTF-8",
+      "user"-> "p",
+      "password" -> "pass",
+      "maximumMessageSize" -> "456",
+      "connectTimeout" -> "789 s"
+    ).asJava)
+    val conf = MysqlAsyncContextConfig(c)
+
+    conf.queryTimeout mustBe Some(123.seconds)
+    conf.connectTimeout mustBe Some(789.seconds)
+    conf.maximumMessageSize mustBe Some(456)
+    conf.charset mustBe Some(Charset.forName("UTF-8"))
+    conf.host mustBe Some("github.com")
+    conf.port mustBe Some(5233)
+    conf.user mustBe Some("p")
+    conf.password mustBe Some("pass")
   }
 
 }
