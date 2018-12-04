284a9436d17bae6f34755514236a34fefb8d9c1f
Flavio W. Brasil <fwbrasil@users.noreply.github.com>
2018-12-04 11:33:42 -0800

    quill-spark: escape strings (#1239)

--- quill-spark/src/main/scala/io/getquill/context/spark/SparkDialect.scala
+++ quill-spark/src/main/scala/io/getquill/context/spark/SparkDialect.scala
@@ -18,6 +18,7 @@ import io.getquill.context.sql.norm.SqlNormalize
 import io.getquill.idiom.StatementInterpolator._
 import io.getquill.idiom.Token
 import io.getquill.util.Messages.trace
+import io.getquill.ast.Constant
 
 class SparkDialect extends SqlIdiom {
 
@@ -75,9 +76,10 @@ class SparkDialect extends SqlIdiom {
   }
 
   override implicit def valueTokenizer(implicit astTokenizer: Tokenizer[Ast], strategy: NamingStrategy): Tokenizer[Value] = Tokenizer[Value] {
-    case Tuple(values)     => stmt"(${values.token})"
-    case CaseClass(values) => stmt"${values.map({ case (prop, value) => stmt"${value.token} ${prop.token}".token }).token}"
-    case other             => super.valueTokenizer.token(other)
+    case Constant(v: String) => stmt"'${v.replaceAll("""[\\']""", """\\$0""").token}'"
+    case Tuple(values)       => stmt"(${values.token})"
+    case CaseClass(values)   => stmt"${values.map({ case (prop, value) => stmt"${value.token} ${prop.token}".token }).token}"
+    case other               => super.valueTokenizer.token(other)
   }
 
   override protected def tokenizeGroupBy(values: Ast)(implicit astTokenizer: Tokenizer[Ast], strategy: NamingStrategy): Token =
