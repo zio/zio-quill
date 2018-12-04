499fb675559c2063ebd8845c246a8a9159468aa9
fwbrasil <fwbrasil@gmail.com>
2018-12-03 23:29:13 -0800

    spark 2.4.0 with scala 2.12

--- quill-spark/src/main/scala/io/getquill/context/spark/SparkDialect.scala
+++ quill-spark/src/main/scala/io/getquill/context/spark/SparkDialect.scala
@@ -11,7 +11,7 @@ import io.getquill.ast.StringOperator
 import io.getquill.ast.Tuple
 import io.getquill.ast.Value
 import io.getquill.ast.CaseClass
-import io.getquill.context.spark.norm.{EscapeQuestionMarks, ExpandEntityIds}
+import io.getquill.context.spark.norm.{ EscapeQuestionMarks, ExpandEntityIds }
 import io.getquill.context.sql.SqlQuery
 import io.getquill.context.sql.idiom.SqlIdiom
 import io.getquill.context.sql.norm.SqlNormalize
