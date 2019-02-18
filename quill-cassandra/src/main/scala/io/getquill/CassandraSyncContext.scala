WayneWang12/quill

change modules to: 1. quill-cassandra, basic cassandra driver. 2. quill-cassandra-datastax that depends on quill-cassandra and uses the datastax driver

quill-cassandra-datastax/src/main/scala/io/getquill/CassandraSyncContext.scala
@@ -1,11 +1,11 @@
 package io.getquill
 
-import com.typesafe.config.Config
-import io.getquill.util.{ ContextLogger, LoadConfig }
-import io.getquill.context.cassandra.CassandraSessionContext
-import scala.collection.JavaConverters._
 import com.datastax.driver.core.Cluster
+import com.typesafe.config.Config
 import io.getquill.monad.SyncIOMonad
+import io.getquill.util.{ ContextLogger, LoadConfig }
+
+import scala.collection.JavaConverters._
 
 class CassandraSyncContext[N <: NamingStrategy](
   naming:                     N,
@@ -13,7 +13,7 @@ class CassandraSyncContext[N <: NamingStrategy](
   keyspace:                   String,
   preparedStatementCacheSize: Long
 )
-  extends CassandraSessionContext[N](naming, cluster, keyspace, preparedStatementCacheSize)
+  extends CassandraDatastaxSessionContext[N](naming, cluster, keyspace, preparedStatementCacheSize)
   with SyncIOMonad {
 
   def this(naming: N, config: CassandraContextConfig) = this(naming, config.cluster, config.keyspace, config.preparedStatementCacheSize)

zio/zio-quill

quill-cassandra/src/main/scala/io/getquill/CassandraSyncContext.scala
@@ -1,11 +1,11 @@
 package io.getquill
 
-import com.typesafe.config.Config
-import io.getquill.util.{ ContextLogger, LoadConfig }
-import io.getquill.context.cassandra.CassandraSessionContext
-import scala.collection.JavaConverters._
 import com.datastax.driver.core.Cluster
+import com.typesafe.config.Config
 import io.getquill.monad.SyncIOMonad
+import io.getquill.util.{ ContextLogger, LoadConfig }
+
+import scala.collection.JavaConverters._
 
 class CassandraSyncContext[N <: NamingStrategy](
   naming:                     N,
@@ -13,7 +13,7 @@ class CassandraSyncContext[N <: NamingStrategy](
   keyspace:                   String,
   preparedStatementCacheSize: Long
 )
-  extends CassandraSessionContext[N](naming, cluster, keyspace, preparedStatementCacheSize)
+  extends CassandraClusterSessionContext[N](naming, cluster, keyspace, preparedStatementCacheSize)
   with SyncIOMonad {
 
   def this(naming: N, config: CassandraContextConfig) = this(naming, config.cluster, config.keyspace, config.preparedStatementCacheSize)