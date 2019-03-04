7f0e1e15f8467e7ec5110b9b0e496762e5dba886
deusaquilus <deusaquilus@gmail.com>
2019-03-04 17:07:29 -0500

    check mem

--- build/build.sh
+++ build/build.sh
@@ -4,6 +4,11 @@ set -e
 
 function show_mem() {
     free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
+
+    echo "===== Free Memory Stats Start ====="
+    free
+    sleep 2
+    echo "===== Free Memory Stats End ====="
 }
 export -f show_mem
 
