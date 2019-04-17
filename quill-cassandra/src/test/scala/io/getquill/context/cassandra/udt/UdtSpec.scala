8f9e3af076b40536f10e7d4783c47e92bfa86a4b
mentegy <mentegy@gmail.com>
2019-04-17 20:49:17 +0300

    Make UDT to support collections

--- quill-cassandra/src/test/scala/io/getquill/context/cassandra/udt/UdtSpec.scala
+++ quill-cassandra/src/test/scala/io/getquill/context/cassandra/udt/UdtSpec.scala
@@ -5,5 +5,13 @@ import io.getquill.context.cassandra.Udt
 
 trait UdtSpec extends Spec {
   case class Name(firstName: String, lastName: Option[String]) extends Udt
-  case class Personal(number: Int, street: String, name: Name, optName: Option[Name]) extends Udt
+  case class Personal(
+    number:  Int,
+    street:  String,
+    name:    Name,
+    optName: Option[Name],
+    list:    List[String],
+    sets:    Set[Int],
+    map:     Map[Int, String]
+  ) extends Udt
 }
