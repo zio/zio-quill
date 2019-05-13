60d2acd380628ed81ce5f5ac6295ad2638cf0108
Alexander Ioffe <deusaquilus@gmail.com>
2019-05-13 12:43:30 -0400

    Try only base-modules publish

--- build/release.sh
+++ build/release.sh
@@ -33,11 +33,11 @@ then
 
     elif [[ $TRAVIS_BRANCH == "master" ]]
     then
-        $SBT_2_12 publish
-        $SBT_2_11 publish
+        $SBT_2_12 -Dmodules=base publish
+        $SBT_2_11 -Dmodules=base publish
     else
         echo "version in ThisBuild := \"$TRAVIS_BRANCH-SNAPSHOT\"" > version.sbt
-        $SBT_2_12 publish
-        $SBT_2_11 publish
+        $SBT_2_12 -Dmodules=base publish
+        $SBT_2_11 -Dmodules=base publish
     fi
 fi
