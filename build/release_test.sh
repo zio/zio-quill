c8dcbe61e1aacfcd531389a51244e75b914db159
deusaquilus <deusaquilus@gmail.com>
2019-05-12 21:09:36 -0400

    debugging build

---
+++ build/release_test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e # Any subsequent(*) commands which fail will cause the shell script to exit immediately
+
+SBT_2_12="sbt ++2.12.6 -Doracle=true"
+SBT_2_11="sbt ++2.11.12 -Doracle=true"
+
+echo $SBT_CMD
+
+openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in ./build/secring.gpg.enc -out local.secring.gpg -d
+openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in ./build/pubring.gpg.enc -out local.pubring.gpg -d
+openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in ./build/credentials.sbt.enc -out local.credentials.sbt -d
+openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in ./build/deploy_key.pem.enc -out local.deploy_key.pem -d
+
+$SBT_2_12 publish
+$SBT_2_11 publish
