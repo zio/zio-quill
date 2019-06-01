e7d461df6a847657fc38d3a9f0ab4b20567e9530
Matthew de Detrich <matthew.de.detrich@zalando.de>
2019-06-01 23:54:50 +0200

    Explicitly pass AsyncContext type params

---
+++ quill-async-mysql/src/test/scala/io/getquill/TypeParamExtensionTest.scala
@@ -0,0 +1,14 @@
+package io.getquill
+
+import io.getquill.context.Context
+
+// Testing we are passing type params explicitly into AsyncContext, otherwise
+// this file will fail to compile
+
+trait BaseExtensions {
+  val context: Context[MySQLDialect, _]
+}
+
+trait AsyncExtensions extends BaseExtensions {
+  override val context: MysqlAsyncContext[_]
+}
