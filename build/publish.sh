32c5a59a36802fa8ad8a4a339a66eba9d35160a8
Alexander Ioffe <deusaquilus@gmail.com>
2019-10-07 12:26:56 -0400

    List Keys

--- build/publish.sh
+++ build/publish.sh
@@ -23,6 +23,9 @@ then
         eval "$(ssh-agent -s)"
         chmod 600 local.deploy_key.pem
         ssh-add local.deploy_key.pem
+        echo "====== Listing Keys ======"
+        gpg --list-keys
+        
         git config --global user.name "Quill CI"
         git config --global user.email "quillci@getquill.io"
         git remote set-url origin git@github.com:getquill/quill.git
