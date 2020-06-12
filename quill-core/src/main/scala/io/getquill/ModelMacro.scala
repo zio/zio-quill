cbb3139f5951e58e039dbdef80aed0742c2a95a6
deusaquilus <deusaquilus@gmail.com>
2020-06-12 00:46:23 -0400

    Add default implementations so Query can be more easily inherited from Dotty

--- quill-core/src/main/scala/io/getquill/ModelMacro.scala
+++ quill-core/src/main/scala/io/getquill/ModelMacro.scala
@@ -2,13 +2,14 @@ package io.getquill
 
 import scala.language.experimental.macros
 import io.getquill.dsl.QueryDslMacro
+import io.getquill.quotation.NonQuotedException
 
 sealed trait EntityQuery[T]
   extends EntityQueryModel[T] {
 
-  override def withFilter(f: T => Boolean): EntityQuery[T]
-  override def filter(f: T => Boolean): EntityQuery[T]
-  override def map[R](f: T => R): EntityQuery[R]
+  override def withFilter(f: T => Boolean): EntityQuery[T] = NonQuotedException()
+  override def filter(f: T => Boolean): EntityQuery[T] = NonQuotedException()
+  override def map[R](f: T => R): EntityQuery[R] = NonQuotedException()
 
   override def insert(value: T): Insert[T] = macro QueryDslMacro.expandInsert[T]
   def insert(f: (T => (Any, Any)), f2: (T => (Any, Any))*): Insert[T]
