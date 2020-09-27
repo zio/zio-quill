34668d5339aeb5c0f189ea12a102861f45fa0020
Alexander Ioffe <deusaquilus@gmail.com>
2020-09-27 03:44:34 -0400

    Decrease BooleanLiteralSupport code copying

--- quill-sql-portable/src/main/scala/io/getquill/sql/idiom/BooleanLiteralSupport.scala
+++ quill-sql-portable/src/main/scala/io/getquill/sql/idiom/BooleanLiteralSupport.scala
@@ -2,42 +2,18 @@ package io.getquill.sql.idiom
 
 import io.getquill.NamingStrategy
 import io.getquill.ast._
-import io.getquill.context.sql.idiom.{ SqlIdiom, VerifySqlQuery }
-import io.getquill.context.sql.norm.{ ExpandNestedQueries, SqlNormalize }
+import io.getquill.context.sql.idiom.SqlIdiom
+import io.getquill.context.sql.norm.SqlNormalize
 import io.getquill.idiom.StatementInterpolator._
-import io.getquill.idiom.{ Statement, StringToken }
+import io.getquill.idiom.StringToken
+import io.getquill.norm.{ ConcatBehavior, EqualityBehavior }
 import io.getquill.quat.Quat
-import io.getquill.sql.norm.{ RemoveExtraAlias, RemoveUnusedSelects, VendorizeBooleans }
-import io.getquill.util.Messages
-import io.getquill.util.Messages.{ fail, trace }
+import io.getquill.sql.norm.VendorizeBooleans
 
 trait BooleanLiteralSupport extends SqlIdiom {
 
-  override def translate(ast: Ast)(implicit naming: NamingStrategy): (Ast, Statement) = {
-    val normalizedAst = VendorizeBooleans(SqlNormalize(ast))
-    implicit val tokernizer = defaultTokenizer
-
-    val token =
-      normalizedAst match {
-        case q: Query =>
-          val sql = querifyAst(q)
-          trace("sql")(sql)
-          VerifySqlQuery(sql).map(fail)
-          val expanded = ExpandNestedQueries(sql)
-          trace("expanded sql")(expanded)
-          val refined = if (Messages.pruneColumns) RemoveUnusedSelects(expanded) else expanded
-          trace("filtered sql (only used selects)")(refined)
-          val cleaned = if (!Messages.alwaysAlias) RemoveExtraAlias(naming)(refined) else refined
-          trace("cleaned sql")(cleaned)
-          val tokenized = cleaned.token
-          trace("tokenized sql")(tokenized)
-          tokenized
-        case other =>
-          other.token
-      }
-
-    (normalizedAst, stmt"$token")
-  }
+  override def normalizeAst(ast: Ast, concatBehavior: ConcatBehavior, equalityBehavior: EqualityBehavior) =
+    VendorizeBooleans(SqlNormalize(ast, concatBehavior, equalityBehavior))
 
   override implicit def valueTokenizer(implicit astTokenizer: Tokenizer[Ast], strategy: NamingStrategy): Tokenizer[Value] =
     Tokenizer[Value] {
