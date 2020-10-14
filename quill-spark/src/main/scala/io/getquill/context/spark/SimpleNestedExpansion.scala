dac9fdc9eb0347bd8c6752c723262d77d04b851f
Alexander Ioffe <deusaquilus@gmail.com>
2020-10-14 19:14:01 -0400

    Abstract Quat.Product Types from Param/Abstract Types => Spark Star Expansions

--- quill-spark/src/main/scala/io/getquill/context/spark/SimpleNestedExpansion.scala
+++ quill-spark/src/main/scala/io/getquill/context/spark/SimpleNestedExpansion.scala
@@ -27,19 +27,65 @@ object TopLevelExpansion {
           case (name, ast) =>
             SelectValue(ast, Some(name), concat)
         }
-      case SelectValue(Ident(singleFieldName, Quat.Product(fields)), alias, concat) if (length == 1) =>
+
+      // If an ident is the only thing in the select list (and it is not abstract, meaning we know what all of it's fields are),
+      // then expand it directly in the selection clauses. If the ident is abstract however, we do not know all of it's fields
+      // and therefore cannot directly expand it into select-values since there could be fields that we have missed.
+      // The only good option that I have thought of so far, is to expand the Ident in the SparkDialect directly
+      // in the FlattenSqlTokenizer with special handling for length=1 selects
+      case SelectValue(Ident(singleFieldName, q @ Quat.Product(fields)), alias, concat) if (length == 1 && q.tpe == Quat.Product.Type.Concrete) =>
         fields.map {
           case (name, quat) =>
             SelectValue(Property(Ident(singleFieldName, quat), name), Some(name), concat)
         }.toList
+
       // Direct infix select, etc...
-      case other if (length == 1) =>
+      case other @ SelectValue(SingleValuePrimitive(), _, _) if (length == 1) =>
         List(other.copy(alias = Some("single")))
       // Technically this case should not exist, adding it so that the pattern match will have full coverage
       case other =>
         List(other)
     }
   }
+}
+
+/**
+ * In each of these situations, the AST can be expanded with as a single SelectValue (with the 'single' alias)
+ * in a select query. For example, SelectValue(Property(p,name)) would become 'SELECT p.name AS single ...'.
+ * SelectValue(If(foo,bar,baz)) would become 'SELECT CASE WHEN foo THEN bar ELSE baz as single ...',
+ * or SelectValue(NullValue) would become 'SELECT null as single ...'
+ * In each of these cases we can assume the actual selection represents a single value.
+ * The opposite of this if the selection is an Ident. In that case, we can never really assume
+ * that the selection represents a single value (*) so we generally have to do star expansions of various kinds.
+ * This unapplier object is used both here and in the SpartDialect select tokenization.
+ *
+ *  - unless the Ident has a Concrete Quat.Proudct with a single value,
+ * but that has already been expanded into it's composite elements in TopLevelExpanion.apply and the Ident shuold no
+ * longer exist in the select values.
+ *
+ * Technically, all we we need to do here is to check that the ast element is not an ident, however due to previous
+ * issues encountered with surprising use-cases with spark, I have decided to ast least log a warning if a single-value
+ * element in a Spark query SelectValue is not one of the expected use-cases.
+ */
+object SingleValuePrimitive {
+  def unapply(ast: Ast): Boolean =
+    ast match {
+      case _: Constant => true
+      case _: Property => true
+      case NullValue => true
+      case op: OptionOperation if (op.quat.isPrimitive) => true
+      case op: BinaryOperation if (op.quat.isPrimitive) => true
+      case op: UnaryOperation if (op.quat.isPrimitive) => true
+      case op: IterableOperation if (op.quat.isPrimitive) => true
+      case i: Infix if (!i.quat.isInstanceOf[Quat.Product]) => true
+      case l: Lift => true
+      // idents should not be directly tokenized as sql values since they always need to be a start-select in a query
+      case id: Ident => false
+      case _ =>
+        import io.getquill.util.Messages._
+        trace(s"Unaccounted-for primitive SelectValue type: ${qprintCustom(traceQuats = QuatTrace.Full)(ast).plainText}. Assuming it can be used as a value-level placeholder and be aliased as 'single'.", traceType = TraceType.Warning)
+        true
+    }
 }
 
 object SimpleNestedExpansion extends StatelessQueryTransformer {
--- quill-spark/src/main/scala/io/getquill/context/spark/SparkDialect.scala
+++ quill-spark/src/main/scala/io/getquill/context/spark/SparkDialect.scala
@@ -3,7 +3,7 @@ package io.getquill.context.spark
 import io.getquill.NamingStrategy
 import io.getquill.ast.{ Ast, BinaryOperation, CaseClass, Constant, ExternalIdent, Ident, Operation, Property, Query, StringOperator, Tuple, Value }
 import io.getquill.context.spark.norm.EscapeQuestionMarks
-import io.getquill.context.sql.{ SqlQuery }
+import io.getquill.context.sql.{ FlattenSqlQuery, SelectValue, SetOperationSqlQuery, SqlQuery, UnaryOperationSqlQuery }
 import io.getquill.context.sql.idiom.SqlIdiom
 import io.getquill.context.sql.norm.SqlNormalize
 import io.getquill.idiom.StatementInterpolator._
@@ -37,7 +37,7 @@ trait SparkIdiom extends SqlIdiom with CannotReturn { self =>
           val expanded = SimpleNestedExpansion(sql)
           trace("expanded sql")(expanded)
           val tokenized = expanded.token
-          trace("tokenized sql")(tokenized)
+          trace("tokenized sql")(tokenized.toString)
           tokenized
         case other =>
           other.token
@@ -49,8 +49,10 @@ trait SparkIdiom extends SqlIdiom with CannotReturn { self =>
   override def concatFunction = "explode"
 
   override implicit def identTokenizer(implicit astTokenizer: Tokenizer[Ast], strategy: NamingStrategy): Tokenizer[Ident] = Tokenizer[Ident] {
-    case id @ Ident(name, Quat.Product(fields)) =>
+    case id @ Ident(name, q @ Quat.Product(fields)) if (q.tpe == Quat.Product.Type.Concrete) =>
       stmt"struct(${fields.map { case (field, subQuat) => (Property(id, field): Ast) }.toList.token})"
+    case id @ Ident(name, q: Quat.Product) if (q.tpe == Quat.Product.Type.Abstract) =>
+      stmt"struct(${name.token}.*)"
     // Situations where a single ident arise with is a Quat.Value typically only happen when an operation yields a single SelectValue
     // e.g. a concatMap (or aggregation?)
     case Ident(name, Quat.Value) =>
@@ -58,6 +60,61 @@ trait SparkIdiom extends SqlIdiom with CannotReturn { self =>
     case Ident(name, _) =>
       stmt"${name.token}"
   }
+
+  class SparkFlattenSqlQueryTokenizerHelper(q: FlattenSqlQuery)(implicit astTokenizer: Tokenizer[Ast], strategy: NamingStrategy)
+    extends FlattenSqlQueryTokenizerHelper(q)(astTokenizer, strategy) {
+
+    override def selectTokenizer: Token = {
+      // Note that by the time we have reached this point, all Idents representing case classes/tuples in selection have
+      // already been expanded into their composite properties. This is handled via the select.token which delegates
+      // to the list tokenizer which delegates to the select value tokenizer. The remaining cases that need special handling
+      // are when there is a single select-value which represents an Ident that cannot be expanded
+      // (either because it's quat is a placeholder type, or it's quat is abstract), or a single
+      // value-level entity (either a Ident whose quat is Quat.Value) or is a single-value field
+      // e.g. a Property, Constant, etc... as defined by SingleValuePrimitive.
+      // The logic of these cases is simple. Any Ident which whose type is unknown needs to be star-expanded, there
+      // is no other choice since we don't know anything else about it. Any primitive-valued ident also needs to be
+      // star-expanded since we do not know the alias of the actual primitive field (hopefully it is 'single' as defined
+      // by SimpleNestedExpansion but we cannot know for sure).
+      // For any other kind of entity e.g. a Property, etc... we can safely tokenize the select-value whose alias
+      // should hopefully also be 'single'
+      q.select match {
+        case Nil => stmt"*"
+        // If we have encountered a situation where a abstract ident is on the top level and the only select field, (i.e. select.length == 1)
+        // it is a strange situation that needs special handling.
+        // We cannot expand the fields of the ident because there might be fields that we don't know about. We also can't just pass
+        // the ident along since in the SparkDialect it will be rendered as Ident("a") => struct(a.*) which doesn't work in a top-level select
+        // (i.e. you can't do "select struct(a.*) from ..." since on the top level the fields must be expanded.)
+        // The only good option that I have thought of so far, is to expand the Ident in the SparkDialect directly
+        // in the FlattenSqlTokenizer with special handling for length=1 selects
+        case List(SelectValue(Ident(a, q @ Quat.Product(_)), _, _)) if (q.tpe == Quat.Product.Type.Abstract) =>
+          stmt"${a.token}.*"
+        // it is an ident but somehow it's type is not known
+        case List(SelectValue(Ident(a, Quat.Placeholder(_)), _, _)) =>
+          stmt"${a.token}.*"
+        // It is an ident but actually it repsents a single sql-level value
+        case List(SelectValue(Ident(a, _: Quat.Primitive), _, _)) =>
+          stmt"${a.token}.*"
+        // If the selection is a single value e.g. SelectValue(prop.value), SelectValue(Constant) return it right here as a SingleValuePrimitive
+        case sv @ List(SelectValue(a @ SingleValuePrimitive(), _, _)) =>
+          sv.token
+        // Otherwise it should have multiple values.
+        // TODO Maybe we should even introduce an exception here because all the support single-value selects have already been enumerated
+        case _ => q.select.token
+      }
+    }
+
+  }
+
+  override implicit def sqlQueryTokenizer(implicit astTokenizer: Tokenizer[Ast], strategy: NamingStrategy): Tokenizer[SqlQuery] = Tokenizer[SqlQuery] {
+    case q: FlattenSqlQuery =>
+      new SparkFlattenSqlQueryTokenizerHelper(q).apply
+    case SetOperationSqlQuery(a, op, b) =>
+      stmt"(${a.token}) ${op.token} (${b.token})"
+    case UnaryOperationSqlQuery(op, q) =>
+      stmt"SELECT ${op.token} (${q.token})"
+  }
+
   override implicit def propertyTokenizer(implicit astTokenizer: Tokenizer[Ast], strategy: NamingStrategy): Tokenizer[Property] = {
     def path(ast: Ast): Token =
       ast match {
