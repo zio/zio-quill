3315a3b3023b35dd207d1524e5c78c05f14fea3d
Alexander Ioffe <deusaquilus@gmail.com>
2021-05-31 09:15:01 -0400

    New asDao helper for ZIO contexts

--- quill-jdbc-zio/src/test/scala/io/getquill/examples/PlainAppDataSource.scala
+++ quill-jdbc-zio/src/test/scala/io/getquill/examples/PlainAppDataSource.scala
@@ -1,11 +1,17 @@
 package io.getquill.examples
 
 import com.zaxxer.hikari.HikariDataSource
-import io.getquill.context.ZioJdbc.QDataSource
+import io.getquill.context.ZioJdbc.{ QConnection, QDataSource }
 import io.getquill.util.LoadConfig
 import io.getquill.{ JdbcContextConfig, Literal, PostgresZioJdbcContext }
 import zio.console.putStrLn
 import zio.Runtime
+import zio._
+import zio.blocking.Blocking
+
+import java.io.Closeable
+import java.sql.{ Connection, SQLException }
+import javax.sql.DataSource
 
 object PlainAppDataSource {
 
@@ -13,6 +19,29 @@ object PlainAppDataSource {
   import MyPostgresContext._
 
   case class Person(name: String, age: Int)
+
+  implicit class RunExt[E <: Throwable, T](run: ZIO[Has[Connection] with Blocking, E, T]) {
+    def asDao =
+      for {
+        ds <- ZIO.environment[Has[DataSource with Closeable] with Blocking]
+        conn <- QDataSource.toConnection.build.useNow.provide(ds)
+        result <- run.provide(conn)
+      } yield result
+
+    def asDao(ds: DataSource with Closeable) =
+      for {
+        block <- ZIO.environment[Blocking]
+        result <- run.asDao.provide(Has(ds) ++ block)
+      } yield result
+  }
+
+  implicit class RunExtDs[E <: Throwable, T](run: ZIO[Has[DataSource with Closeable] with Blocking, E, T]) {
+    def provideDataSource(ds: DataSource with Closeable) =
+      for {
+        block <- ZIO.environment[Blocking]
+        result <- run.provide(Has(ds) ++ block)
+      } yield result
+  }
 
   def config = JdbcContextConfig(LoadConfig("testPostgresDB")).dataSource
 
@@ -23,12 +52,22 @@ object PlainAppDataSource {
     val people = quote {
       query[Person].filter(p => p.name == "Alex")
     }
-    val qzio =
-      MyPostgresContext.run(people)
-        .tap(result => putStrLn(result.toString))
-        .provideCustomLayer(zioConn)
+    //val qzio =
+    //  MyPostgresContext.run(people).asDao.provide(QDataSource.fromDataSource(ds))
 
-    Runtime.default.unsafeRun(qzio)
+    val qzio1 =
+      MyPostgresContext.run(people).asInstanceOf[ZIO[QConnection, SQLException, Throwable]]
+
+    val ds: DataSource with Closeable = null
+    val qzio2 = qzio1.asDao.provideCustomLayer(QDataSource.fromDataSource(ds))
+
+    val qzio22 = qzio1.asDao.provideDataSource(ds)
+    val qzio23 = qzio1.asDao(ds)
+
+    //.tap(result => putStrLn(result.toString))
+    //.provideCustomLayer(zioConn)
+
+    Runtime.default.unsafeRun(qzio2)
     ()
   }
 }
