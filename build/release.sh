e4a3024121a760bb367290d7d00bc225504dc2fc
Matthew de Detrich <mdedetrich@gmail.com>
2021-07-04 23:39:45 +0200

    Remove various travism's from codebase

--- build/release.sh
+++ build/release.sh
@@ -28,7 +28,7 @@ else
 fi
 
 echo $SBT_CMD
-if [[ $TRAVIS_PULL_REQUEST == "false" ]]
+if [[ $PULL_REQUEST == "false" ]]
 then
     echo "Export secring"
     openssl aes-256-cbc -md sha256 -salt -pbkdf2 -pass pass:$ENCRYPTION_PASSWORD -in ./build/secring.gpg.enc -out local.secring.gpg -d
@@ -62,16 +62,16 @@ then
     ls -ltr
     sleep 3 # Need to wait until credential files fully written or build fails sometimes
     project_version="v$(cat version.sbt | awk -F'"' '{print $2}')"
-    echo "Detected project_version '$project_version' from SBT Files (on TRAVIS_BRANCH '$TRAVIS_BRANCH')"
+    echo "Detected project_version '$project_version' from SBT Files (on BRANCH '$BRANCH')"
 
     # When an artifact is actually published, a build will go out on the git commit: "Setting version to <YOUR VERSION>".
     # The job before that is the one that creates the vX.X.X tag e.g. v3.0.0. We build and release on that one
     # as well as any branch name 're-release*' in case a build fails and we need to re-publish.
     # (Also note, we could technically use $project_version instead of $(cat version.sbt) but I don't want to change that this time around.)
 
-    if [[ ($TRAVIS_BRANCH == "master" || $TRAVIS_BRANCH == "re-release"*) && $(cat version.sbt) != *"SNAPSHOT"* ]]
+    if [[ ($BRANCH == "master" || $BRANCH == "re-release"*) && $(cat version.sbt) != *"SNAPSHOT"* ]]
     then
-        echo "Release Build for $TRAVIS_BRANCH - Artifact: '$ARTIFACT'"
+        echo "Release Build for $BRANCH - Artifact: '$ARTIFACT'"
         eval "$(ssh-agent -s)"
         chmod 600 local.deploy_key.pem
         ssh-add local.deploy_key.pem
@@ -87,11 +87,11 @@ then
         if [[ $ARTIFACT == "bigdata" ]]; then $SBT_VER -Dmodules=bigdata -DskipPush=true 'release with-defaults'; fi
 
         # Commit next version and tag if we are on the master branch (i.e. not if we are on a re-release)
-        if [[ $TRAVIS_BRANCH == "master" && $ARTIFACT == "publish" ]]; then $SBT_VER -Dmodules=none 'release with-defaults default-tag-exists-answer o'; fi
+        if [[ $BRANCH == "master" && $ARTIFACT == "publish" ]]; then $SBT_VER -Dmodules=none 'release with-defaults default-tag-exists-answer o'; fi
 
-    elif [[ $TRAVIS_BRANCH == "master" && $(cat version.sbt) == *"SNAPSHOT"* ]]
+    elif [[ $BRANCH == "master" && $(cat version.sbt) == *"SNAPSHOT"* ]]
     then
-        echo "Master Non-Release Build for $TRAVIS_BRANCH - Artifact: '$ARTIFACT'"
+        echo "Master Non-Release Build for $BRANCH - Artifact: '$ARTIFACT'"
         if [[ $ARTIFACT == "base" ]]; then    $SBT_VER -Dmodules=base publish; fi
         if [[ $ARTIFACT == "db" ]]; then      $SBT_VER -Dmodules=db publish; fi
         if [[ $ARTIFACT == "js" ]]; then      $SBT_VER -Dmodules=js publish; fi
@@ -104,10 +104,10 @@ then
 
     # If we are a branch build publish it. We are assuming this script does NOT become activated in pulls requests
     # and that condition is done at a higher level then this script
-    elif [[ $TRAVIS_BRANCH != "master" ]]
+    elif [[ $BRANCH != "master" ]]
     then
-        echo "Branch build for $TRAVIS_BRANCH - Artifact: '$ARTIFACT'"
-        echo "version in ThisBuild := \"$TRAVIS_BRANCH-SNAPSHOT\"" > version.sbt
+        echo "Branch build for $BRANCH - Artifact: '$ARTIFACT'"
+        echo "version in ThisBuild := \"$BRANCH-SNAPSHOT\"" > version.sbt
         if [[ $ARTIFACT == "base" ]]; then    $SBT_VER -Dmodules=base publish; fi
         if [[ $ARTIFACT == "db" ]]; then      $SBT_VER -Dmodules=db publish; fi
         if [[ $ARTIFACT == "js" ]]; then      $SBT_VER -Dmodules=js publish; fi
@@ -119,6 +119,6 @@ then
         if [[ $ARTIFACT == "publish" ]]; then echo "No-Op Publish for Non Release Snapshot Branch"; fi
     else
         VERSION_FILE=$(cat version.sbt)
-        echo "Travis branch was: ${$TRAVIS_BRANCH} and version file is $VERSION_FILE. Not Sure what to do."
+        echo "Github actions branch was: ${$BRANCH} and version file is $VERSION_FILE. Not Sure what to do."
     fi
 fi
