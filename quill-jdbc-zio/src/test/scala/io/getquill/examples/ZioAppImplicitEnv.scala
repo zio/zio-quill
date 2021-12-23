fdc2193e84d8b88796b0bc13c91e9fceaf792c5d
jules Ivanic <jules.ivanic@gmail.com>
2021-12-23 13:02:27 +0800

    Fix compilation

--- quill-jdbc-zio/src/test/scala/io/getquill/examples/ZioAppImplicitEnv.scala
+++ quill-jdbc-zio/src/test/scala/io/getquill/examples/ZioAppImplicitEnv.scala
@@ -1,27 +1,30 @@
 package io.getquill.examples
 
 import io.getquill._
+import io.getquill.context.ZioJdbc.DataSource
 import io.getquill.context.qzio.ImplicitSyntax._
 import io.getquill.util.LoadConfig
 import zio.console.putStrLn
-import zio.{ App, ExitCode, Has, URIO }
-import javax.sql.DataSource
+import zio.{App, ExitCode, Has, IO, URIO}
+
+import java.io.Closeable
+import java.sql.SQLException
 
 object ZioAppImplicitEnv extends App {
 
   object Ctx extends PostgresZioJdbcContext(Literal)
 
-  case class Person(name: String, age: Int)
+  final case class Person(name: String, age: Int)
 
-  def dataSource = JdbcContextConfig(LoadConfig("testPostgresDB")).dataSource
+  def dataSource: DataSource with Closeable = JdbcContextConfig(LoadConfig("testPostgresDB")).dataSource
 
-  case class MyQueryService(ds: DataSource) {
+  final case class MyQueryService(ds: DataSource) {
     import Ctx._
-    implicit val env = Implicit(Has(ds))
+    implicit val env: Implicit[Has[DataSource]] = Implicit(Has(ds))
 
-    val joes = Ctx.run(query[Person].filter(p => p.name == "Joe")).implicitly
-    val jills = Ctx.run(query[Person].filter(p => p.name == "Jill")).implicitly
-    val alexes = Ctx.run(query[Person].filter(p => p.name == "Alex")).implicitly
+    val joes: IO[SQLException, List[Person]] = Ctx.run(query[Person].filter(p => p.name == "Joe")).implicitly
+    val jills: IO[SQLException, List[Person]] = Ctx.run(query[Person].filter(p => p.name == "Jill")).implicitly
+    val alexes: IO[SQLException, List[Person]] = Ctx.run(query[Person].filter(p => p.name == "Alex")).implicitly
   }
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
