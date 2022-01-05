d5d96cc414678f313dd82370c80149c2151a80cd
Alexander Ioffe <deusaquilus@gmail.com>
2022-01-05 16:11:38 -0500

    Improve free variable errors

--- quill-engine/src/main/scala/io/getquill/quotation/FreeVariables.scala
+++ quill-engine/src/main/scala/io/getquill/quotation/FreeVariables.scala
@@ -108,4 +108,20 @@ object FreeVariables {
       case (_, transformer) =>
         transformer.state.free
     }
+
+  def verify(ast: Ast): Either[String, Ast] =
+    apply(ast) match {
+      case free if free.isEmpty => Right(ast)
+      case free =>
+        val firstVar = free.headOption.map(_.name).getOrElse("someVar")
+        Left(s"""
+          |Found the following variables: ${free.map(_.name).toList} that seem to originate outside of a `quote {...}` or `run {...}` block.
+          |Quotes and run blocks cannot use values outside their scope directly (with the exception of inline expressions in Scala 3).
+          |In order to use runtime values in a quotation, you need to lift them, so instead
+          |of this `$firstVar` do this: `lift($firstVar)`.
+          |Here is a more complete example:
+          |Instead of this: `def byName(n: String) = quote(query[Person].filter(_.name == n))`
+          |        Do this: `def byName(n: String) = quote(query[Person].filter(_.name == lift(n)))`
+        """.stripMargin)
+    }
 }
