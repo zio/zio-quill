13723d7c5cb329819fb82f734b81d9306d95dcce
Daave <root@widgetbot.io>
2022-02-16 17:03:04 +0000

    fix: Move fix from non-zio

--- quill-jasync-zio-postgres/src/test/scala/io/getquill/context/zio/jasync/postgres/PostgresAsyncEncodingSpec.scala
+++ quill-jasync-zio-postgres/src/test/scala/io/getquill/context/zio/jasync/postgres/PostgresAsyncEncodingSpec.scala
@@ -82,9 +82,9 @@ class PostgresAsyncEncodingSpec extends EncodingSpec with ZioSpec {
     success must not be empty
   }
 
-  "decodes ZonedDateTime, LocalDate and LocalDateTime types" in {
-    case class DateEncodingTestEntity(v1: LocalDate, v2: LocalDateTime, v3: ZonedDateTime)
-    val entity = DateEncodingTestEntity(LocalDate.now, LocalDateTime.now, ZonedDateTime.now)
+  "decodes LocalDate and LocalDateTime types" in {
+    case class DateEncodingTestEntity(v1: LocalDate, v2: LocalDateTime)
+    val entity = DateEncodingTestEntity(LocalDate.now, LocalDateTime.now)
     val r = for {
       _ <- testContext.run(query[DateEncodingTestEntity].delete)
       _ <- testContext.run(query[DateEncodingTestEntity].insertValue(lift(entity)))
