a8baa301ba766c8c7ddbb76e2faeeb106ebfd560
Erik LaBianca <erik.labianca@gmail.com>
2022-05-19 21:50:37 -0400

    Clear out NoramlizeCache if size exceeds 100 entries

--- quill-engine/src/main/scala/io/getquill/norm/NormalizeCaching.scala
+++ quill-engine/src/main/scala/io/getquill/norm/NormalizeCaching.scala
@@ -4,6 +4,7 @@ import io.getquill.ast.Ast
 import java.util.concurrent.ConcurrentHashMap
 
 object NormalizeCaching {
+  private val MAX_ENTRIES = 100
   private val cache = new ConcurrentHashMap[Ast, Ast]
 
   def apply(f: Ast => Ast): Ast => Ast = { ori =>
@@ -12,6 +13,7 @@ object NormalizeCaching {
     val normalized = if (cachedR != null) {
       cachedR
     } else {
+      if(cache.size > MAX_ENTRIES) cache.clear()
       val r = f(stablized)
       cache.put(stablized, r)
       r
