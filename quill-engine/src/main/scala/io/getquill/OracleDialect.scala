20c7d912723859ffdd311b3504b2fe9cffc97f88
chriseteka <christopher.eteka@lunatech.nl>
2022-05-26 16:55:54 +0200

    Removed 'AS' from OracleDialect

--- quill-engine/src/main/scala/io/getquill/OracleDialect.scala
+++ quill-engine/src/main/scala/io/getquill/OracleDialect.scala
@@ -58,7 +58,7 @@ trait OracleDialect
     }
 
   override implicit def sourceTokenizer(implicit astTokenizer: Tokenizer[Ast], strategy: NamingStrategy): Tokenizer[FromContext] = Tokenizer[FromContext] {
-    case QueryContext(query, alias) => stmt"(${query.token}) ${strategy.default(alias).token}"
+    case InfixContext(infix, alias) => stmt"(${(infix: Ast).token}) ${tokenizeTableAlias(strategy, alias).token}"
     case other                      => super.sourceTokenizer.token(other)
   }
 
