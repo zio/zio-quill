23ff6c3edc43e79ec13df15bf57405dd86027136
Alexander Ioffe <deusaquilus@gmail.com>
2022-06-13 00:04:19 -0400

    Add support for SSL Client connections

--- quill-jasync-postgres/src/test/scala/io/getquill/PostgresJAsyncContextConfigSpec.scala
+++ quill-jasync-postgres/src/test/scala/io/getquill/PostgresJAsyncContextConfigSpec.scala
@@ -9,13 +9,25 @@ import com.typesafe.config.{ ConfigFactory, ConfigValueFactory }
 class PostgresJAsyncContextConfigSpec extends Spec {
 
   "parses ssl config" in {
-    val config = ConfigFactory.empty()
+    val config = ConfigFactory
+      .empty()
       .withValue("user", ConfigValueFactory.fromAnyRef("user"))
       .withValue("port", ConfigValueFactory.fromAnyRef(5432))
       .withValue("host", ConfigValueFactory.fromAnyRef("host"))
       .withValue("sslmode", ConfigValueFactory.fromAnyRef("require"))
-      .withValue("sslrootcert", ConfigValueFactory.fromAnyRef("./file.crt"))
-    val context = PostgresJAsyncContextConfig(config)
-    context.connectionPoolConfiguration.getSsl mustEqual new SSLConfiguration(Mode.Require, new File("./file.crt"), null, null)
+      .withValue(
+        "sslrootcert",
+        ConfigValueFactory.fromAnyRef("./server-ca.crt")
+      )
+      .withValue("sslcert", ConfigValueFactory.fromAnyRef("./client-cert.pem"))
+      .withValue("sslkey", ConfigValueFactory.fromAnyRef("./client-key.pk8"))
+
+    val context = new PostgresJAsyncContextConfig(config)
+    context.connectionPoolConfiguration.getSsl mustEqual new SSLConfiguration(
+      Mode.Require,
+      new File("./server-ca.crt"),
+      new File("./client-cert.pem"),
+      new File("./client-key.pk8")
+    )
   }
 }
