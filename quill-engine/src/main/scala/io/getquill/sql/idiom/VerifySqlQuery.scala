decb6495617a154d7d7d9a35aa673883d8ed6f93
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-09-20 16:27:04 -0400

    spelling: throw
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- quill-engine/src/main/scala/io/getquill/sql/idiom/VerifySqlQuery.scala
+++ quill-engine/src/main/scala/io/getquill/sql/idiom/VerifySqlQuery.scala
@@ -141,9 +141,9 @@ object VerifySqlQuery {
           throw new IllegalArgumentException("Cannot use Option.tableExists on a table or embedded case class")
 
         case cond: If if cond.`then`.isInstanceOf[Quat.Product] =>
-          throw throw new IllegalArgumentException("Cannot use table or embedded case class as a result of a condition")
+          throw new IllegalArgumentException("Cannot use table or embedded case class as a result of a condition")
         case cond: If if cond.`else`.isInstanceOf[Quat.Product] =>
-          throw throw new IllegalArgumentException("Cannot use table or embedded case class as a result of a condition")
+          throw new IllegalArgumentException("Cannot use table or embedded case class as a result of a condition")
 
         case cond: If => checkIllegalIdents(cond.condition)
         case other    => None
