d272a038935cfa2687b18bee32477917781e7631
Alexander Ioffe <deusaquilus@gmail.com>
2022-10-07 01:32:51 -0400

    JDBC DataSource should not need to be closeable

--- quill-jdbc/src/main/scala/io/getquill/PostgresJdbcContext.scala
+++ quill-jdbc/src/main/scala/io/getquill/PostgresJdbcContext.scala
@@ -7,7 +7,7 @@ import com.typesafe.config.Config
 import io.getquill.context.jdbc.{ JdbcContext, PostgresJdbcContextBase }
 import io.getquill.util.LoadConfig
 
-class PostgresJdbcContext[+N <: NamingStrategy](val naming: N, val dataSource: DataSource with Closeable)
+class PostgresJdbcContext[+N <: NamingStrategy](val naming: N, val dataSource: DataSource)
   extends JdbcContext[PostgresDialect, N]
   with PostgresJdbcContextBase[PostgresDialect, N] {
   override val idiom: PostgresDialect = PostgresDialect
