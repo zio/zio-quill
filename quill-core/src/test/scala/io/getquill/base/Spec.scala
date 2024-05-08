5d13cfe118f30e3e1dcfbd1adfc6ae7683399122
Jules Ivanic <jules.ivanic@gmail.com>
2024-05-08 16:31:58 +0800

    Drop Monix support (#3033)

--- quill-core/src/test/scala/io/getquill/base/Spec.scala
+++ quill-core/src/test/scala/io/getquill/base/Spec.scala
@@ -1,35 +1,33 @@
 package io.getquill.base
 
-import io.getquill.{BatchActionMirrorGeneric, BatchActionReturningMirrorGeneric}
 import io.getquill.ast.{Ident, StatelessTransformer}
+import io.getquill.context.ExecutionType
 import io.getquill.norm.capture.TemporaryIdent
 import io.getquill.quat.Quat
+import io.getquill.{BatchActionMirrorGeneric, BatchActionReturningMirrorGeneric}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
-import scala.language.higherKinds
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 
 abstract class Spec extends AnyFreeSpec with Matchers with BeforeAndAfterAll {
-  val QV                                = Quat.Value
-  val QEP                               = Quat.Product.empty("Product")
-  def QP(name: String, fields: String*) = Quat.LeafProduct(name, fields: _*)
+  val QV: Quat.Value.type                             = Quat.Value
+  val QEP: Quat.Product                               = Quat.Product.empty("Product")
+  def QP(name: String, fields: String*): Quat.Product = Quat.LeafProduct(name, fields: _*)
 
   // Used by various tests to replace temporary idents created by AttachToEntity with 'x'
-  val replaceTempIdent = new StatelessTransformer {
+  val replaceTempIdent: StatelessTransformer = new StatelessTransformer {
     override def applyIdent(id: Ident): Ident =
       id match {
-        case TemporaryIdent(tid) =>
-          Ident("x", id.quat)
-        case _ =>
-          id
+        case TemporaryIdent(_) => Ident("x", id.quat)
+        case _                 => id
       }
   }
 
   implicit class QuatOps(quat: Quat) {
-    def productOrFail() =
+    def productOrFail(): Quat.Product =
       quat match {
         case p: Quat.Product => p
         case _               => throw new IllegalArgumentException(s"The quat ${quat} is expected to be a product but is not")
@@ -39,7 +37,7 @@ abstract class Spec extends AnyFreeSpec with Matchers with BeforeAndAfterAll {
   def await[T](f: Future[T]): T = Await.result(f, Duration.Inf)
 
   implicit class MirrorContextOps(m: BatchActionMirrorGeneric[_]) {
-    def triple = {
+    def triple: (String, List[List[Any]], ExecutionType) = {
       if (m.groups.length != 1)
         fail(s"Expected all batch groups per design to only have one root element but has multiple ${m.groups}")
       val (queryString, prepares) = m.groups(0)
@@ -56,7 +54,7 @@ abstract class Spec extends AnyFreeSpec with Matchers with BeforeAndAfterAll {
       )
     }
 
-    def tripleBatchMulti =
+    def tripleBatchMulti: List[(String, List[List[Any]], ExecutionType)] =
       m.groups.map { case (queryString, prepares) =>
         (
           queryString,
@@ -73,7 +71,7 @@ abstract class Spec extends AnyFreeSpec with Matchers with BeforeAndAfterAll {
   }
 
   implicit class MirrorReturningContextOps[E[_]](m: BatchActionReturningMirrorGeneric[_, _, E]) {
-    def triple = {
+    def triple: (String, List[List[Any]], ExecutionType) = {
       if (m.groups.length != 1)
         fail(s"Expected all batch groups per design to only have one root element but has multiple ${m.groups}")
       val (queryString, returnAction, prepares) = m.groups(0)
@@ -90,7 +88,7 @@ abstract class Spec extends AnyFreeSpec with Matchers with BeforeAndAfterAll {
       )
     }
 
-    def tripleBatchMulti =
+    def tripleBatchMulti: List[(String, List[List[Any]], ExecutionType)] =
       m.groups.map { case (queryString, returnAction, prepares) =>
         (
           queryString,
--- quill-jdbc-monix/src/test/scala/io/getquill/sqlserver/sqlserver.scala
+++
@@ -1,8 +0,0 @@
-package io.getquill
-
-import monix.execution.Scheduler
-
-package object sqlserver {
-  private implicit val scheduler = Scheduler.global
-  object testContext extends SqlServerMonixJdbcContext(Literal, "testSqlServerDB") with TestEntities
-}
