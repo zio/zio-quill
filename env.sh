2f1222ed1337da1d50d9d2e788427730e25d4616
Mattia Pitossi <mattia.pitossi@moneyfarm.com>
2025-04-06 12:17:19 +0200

    fix: add ORACLE_DB env

--- env.sh
+++ env.sh
@@ -11,6 +11,7 @@ export ORIENTDB_HOST=127.0.0.1
 export ORIENTDB_PORT=12424
 export ORACLE_HOST=127.0.0.1
 export ORACLE_PORT=11521
+export ORACLE_DB=xe
 
 export JAVA_OPTS="-Dquill.macro.log=false -Xms1024m -Xmx3g -Xss5m -XX:ReservedCodeCacheSize=256m -XX:+TieredCompilation -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC"
 
